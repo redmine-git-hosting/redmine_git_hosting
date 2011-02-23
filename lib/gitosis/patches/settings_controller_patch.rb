78ae97acb08f838163aa95681ae26489083a23f4
Sebastian Harl <sh@teamix.net>
2011-02-23 09:29:10 +0100

    settings_controller_patch.rb: Cleanup.
    Removed/Fixed temporary function name … d'oh!

--- lib/gitosis/patches/settings_controller_patch.rb
+++ lib/gitosis/patches/settings_controller_patch.rb
@@ -3,18 +3,18 @@ module Gitosis
   module Patches
     module SettingsControllerPatch
 
-      def plugin_with_foo
-	if params[:commit] and params[:commit] == 'Apply'
+      def plugin_with_update_repo
+        if params[:commit] and params[:commit] == 'Apply'
           Gitosis.update_repositories(Project.active)
-	end
-	plugin_without_foo
+        end
+        plugin_without_update_repo
       end
 
       def self.included(base)
         base.class_eval do
           unloadable
         end
-        base.send(:alias_method_chain, :plugin, :foo)
+        base.send(:alias_method_chain, :plugin, :update_repo)
       end
     end
   end
