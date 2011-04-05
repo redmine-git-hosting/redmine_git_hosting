a77bff8caa73504919f0a3f75ea3d027a83b6546
Eric Bishop <ericpaulbishop@gmail.com>
2011-04-05 12:50:33 -0400

    send pieces of url beyond git repo name as parameters p1-p3 (since there will be at most 3) to the grack controller

--- app/controllers/grack_controller.rb
+++ app/controllers/grack_controller.rb
@@ -1,6 +1,7 @@
 class GrackController < ApplicationController
   
   def index
+    render :text=>"<html><body>p1=" + params[:p1] + "<br>p2=" + params[:p2] + "<br>p3=" + params[:p3] + "</body></body>\n"
 
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
+#git_repos = Repository.where(:scm_name=>"Git")
 repos = Repository.find(:all)
+
 
 if defined? map
 	map.resources :public_keys, :controller => 'gitosis_public_keys', :path_prefix => 'my'
 	for repo in repos do 
 		if repo.is_a?(Repository::Git)
 			grack_path=repo.url.gsub(/^.*\//, '')
-			map.connect grack_path, :controller => 'grack'
+			map.connect grack_path,                  :controller => 'grack', :p1 => '', :p2 =>'', :p3 =>''
+			map.connect grack_path + "/:p1",         :controller => 'grack', :p2 => '', :p3 =>''
+			map.connect grack_path + "/:p1/:p2",     :controller => 'grack', :p3 => ''
+			map.connect grack_path + "/:p1/:p2/:p3", :controller => 'grack'
 		end
 	end
 else
@@ -14,10 +19,12 @@ else
 		for repo in repos do 
 			if repo.is_a?(Repository::Git)
 				grack_path=repo.url.gsub(/^.*\//, '')
-				map.connect grack_path, :controller => 'grack'
+				map.connect grack_path,                  :controller => 'grack', :p1 => '', :p2 =>'', :p3 =>''
+				map.connect grack_path + "/:p1",         :controller => 'grack', :p2 => '', :p3 =>''
+				map.connect grack_path + "/:p1/:p2",     :controller => 'grack', :p3 => ''
+				map.connect grack_path + "/:p1/:p2/:p3", :controller => 'grack'
 			end
 		end
 	end
 end
-
 
