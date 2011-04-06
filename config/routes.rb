2f07db5b1f18bc011af1840d34676b8781741fe6
Eric Bishop <ericpaulbishop@gmail.com>
2011-04-06 11:25:54 -0400

    send project identifier and repo path to grack controller, in addition to rest of url

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
-#git_repos = Repository.where(:scm_name=>"Git")
-repos = Repository.find(:all)
-
+projects = Project.find(:all)
 
 if defined? map
 	map.resources :public_keys, :controller => 'gitosis_public_keys', :path_prefix => 'my'
-	for repo in repos do 
+	for p in projects do
+	       repo = p.repository	
 		if repo.is_a?(Repository::Git)
-			grack_path=repo.url.gsub(/^.*\//, '')
-			map.connect grack_path,                  :controller => 'grack', :p1 => '', :p2 =>'', :p3 =>''
-			map.connect grack_path + "/:p1",         :controller => 'grack', :p2 => '', :p3 =>''
-			map.connect grack_path + "/:p1/:p2",     :controller => 'grack', :p3 => ''
-			map.connect grack_path + "/:p1/:p2/:p3", :controller => 'grack'
+			repo_path=repo.url.gsub(/^.*\//, '')
+			map.connect repo_path,                  :controller => 'grack', :p1 => '', :p2 =>'', :p3 =>'', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
+			map.connect repo_path + "/:p1",         :controller => 'grack', :p2 => '', :p3 =>'', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
+			map.connect repo_path + "/:p1/:p2",     :controller => 'grack', :p3 => '', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
+			map.connect repo_path + "/:p1/:p2/:p3", :controller => 'grack', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
 		end
 	end
 else
 	ActionController::Routing::Routes.draw do |map|
 		map.resources :public_keys, :controller => 'gitosis_public_keys', :path_prefix => 'my'
-		for repo in repos do 
+		for p in projects do 
+			repo = p.repository
 			if repo.is_a?(Repository::Git)
-				grack_path=repo.url.gsub(/^.*\//, '')
-				map.connect grack_path,                  :controller => 'grack', :p1 => '', :p2 =>'', :p3 =>''
-				map.connect grack_path + "/:p1",         :controller => 'grack', :p2 => '', :p3 =>''
-				map.connect grack_path + "/:p1/:p2",     :controller => 'grack', :p3 => ''
-				map.connect grack_path + "/:p1/:p2/:p3", :controller => 'grack'
+				repo_path=repo.url.gsub(/^.*\//, '')
+				map.connect repo_path,                  :controller => 'grack', :p1 => '', :p2 =>'', :p3 =>'', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
+				map.connect repo_path + "/:p1",         :controller => 'grack', :p2 => '', :p3 =>'', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
+				map.connect repo_path + "/:p1/:p2",     :controller => 'grack', :p3 => '', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
+				map.connect repo_path + "/:p1/:p2/:p3", :controller => 'grack', :id=>"#{p[:identifier]}", :path=>"#{repo_path}"
 			end
 		end
 	end
