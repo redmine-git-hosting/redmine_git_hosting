03e2fb91574441e1b9205ed98c29eee466d7c984
Eric Bishop <ericpaulbishop@gmail.com>
2011-04-13 16:43:14 -0400

    add method to get git repo options given a repository, instantiates the object for a given repository if it doesn't already exist

--- app/models/git_repo_hosting_options.rb
+++ app/models/git_repo_hosting_options.rb
@@ -1,5 +1,23 @@
 class GitRepoHostingOptions < ActiveRecord::Base
 
-  belongs_to :repository
-  
+	belongs_to :repository
+
+	def self.find_for_repo(repo)
+		gitOps = nil
+		if repo
+			gitOpList = self.find(:all, :conditions=>{:repository_id=>repo.id})
+			if(gitOpList.length == 0)
+				gitOps = GitRepoHostingOptions.new do |g|
+					g.repository_id=repo[:id]
+				end	
+			else
+				gitOps = gitOpList[0]	
+			end
+		end
+		return gitOps
+	end
+
+	def to_s
+		self.repository_id.to_s
+	end  
 end
