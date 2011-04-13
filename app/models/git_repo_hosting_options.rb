a481f02189fe556ce49410c664c5de3e40e5ddce
Eric Bishop <ericpaulbishop@gmail.com>
2011-04-13 16:56:06 -0400

    make sure repo is a git repository before initializing GitRepoHostingOptions object for it

--- app/models/git_repo_hosting_options.rb
+++ app/models/git_repo_hosting_options.rb
@@ -5,13 +5,15 @@ class GitRepoHostingOptions < ActiveRecord::Base
 	def self.find_for_repo(repo)
 		gitOps = nil
 		if repo
-			gitOpList = self.find(:all, :conditions=>{:repository_id=>repo.id})
-			if(gitOpList.length == 0)
-				gitOps = GitRepoHostingOptions.new do |g|
-					g.repository_id=repo[:id]
-				end	
-			else
-				gitOps = gitOpList[0]	
+			if repo.is_a?(Repository::Git)
+				gitOpList = self.find(:all, :conditions=>{:repository_id=>repo.id})
+				if(gitOpList.length == 0)
+					gitOps = GitRepoHostingOptions.new do |g|
+						g.repository_id=repo[:id]
+					end	
+				else
+					gitOps = gitOpList[0]	
+				end
 			end
 		end
 		return gitOps
