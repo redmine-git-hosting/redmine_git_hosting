222d72700639fe6c41ad37f4e1e5e4e1ba6a1ef7
Eric <ericpaulbishop@gmail.com>
2011-04-13 22:18:23 -0400

    fix formatting of new git repository options

--- lib/gitolite/patches/repositories_helper_patch.rb
+++ lib/gitolite/patches/repositories_helper_patch.rb
@@ -3,9 +3,6 @@ module Gitolite
 	module Patches
 		module RepositoriesHelperPatch
 			def git_field_tags_with_hosting_configuration(form, repository)
-				
-				left_style="style='display:block;float:left;width:150px;margin-right:5px'"
-				right_style="style='display:block;margin-left:210px;'"
 				git_daemon_label = "Git Daemon"
 				git_http_pull_label = "Git Smart HTTP (pull)"
 				git_http_push_label = "Git Smart HTTP (push)"
@@ -17,11 +14,11 @@ module Gitolite
 				git_daemon_pull_options = "<option value='0'>#{disabled_label}</option>" + (@project.is_public ? "<option value='1'>#{enabled_label}</option>" : "") 
 				git_http_options = "<option value='2'>#{https_and_http_label}</option><option value='1'>#{https_only_label}</option><option value='0'>#{disabled_label}</option>"
 				
-				git_daemon_pull_control = "<label #{left_style} for='git_daemon_pull'>#{git_daemon_label}:</label><select id='git_daemon_pull' #{right_style}>" + git_daemon_pull_options + "</select>";
-				git_http_pull_control = "<label #{left_style} for='git_http_pull'>#{git_http_pull_label}:</label><select id='git_http_pull' #{right_style}>" + git_http_options + "</select>";
-				git_http_push_control = "<label #{left_style} for='git_http_pull'>#{git_http_push_label}:</label><select id='git_http_pull' #{right_style}>" + git_http_options + "</select>";
+				git_daemon_pull_control = "<label for='git_daemon_pull'>#{git_daemon_label}:</label><select id='git_daemon_pull'>" + git_daemon_pull_options + "</select>";
+				git_http_pull_control = "<label for='git_http_pull'>#{git_http_pull_label}:</label><select id='git_http_pull'>" + git_http_options + "</select>";
+				git_http_push_control = "<label for='git_http_pull'>#{git_http_push_label}:</label><select id='git_http_pull'>" + git_http_options + "</select>";
 
-				return git_daemon_pull_control + git_http_pull_control + git_http_push_control	
+				return "\n<p>" + git_daemon_pull_control + "</p>\n<p>" +  git_http_pull_control + "</p>\n<p>" + git_http_push_control + "</p>\n"
 
 
 			end
