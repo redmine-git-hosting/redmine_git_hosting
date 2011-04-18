ca5a45e1da8f9b2f559f15c0ad1dcfca5144cb16
Eric <ericpaulbishop@gmail.com>
2011-04-18 08:22:04 -0400

    redirect properl after done editing key

--- app/controllers/gitolite_public_keys_controller.rb
+++ app/controllers/gitolite_public_keys_controller.rb
@@ -5,22 +5,22 @@ class GitolitePublicKeysController < ApplicationController
 	before_filter :set_user_variable
 	before_filter :find_gitolite_public_key, :except => [:index, :new, :create]
 
-	def index
-		@status = if (session[:gitolite_public_key_filter_status]=params[:status]).nil?
-			GitolitePublicKey::STATUS_ACTIVE
-		elsif params[:status].blank?
-				nil
-		else 
-			params[:status] == "true"
-		end
-		c = ARCondition.new(!@status.nil? ? ["active=?", @status] : nil)
-
-		@gitolite_public_keys = @user.gitolite_public_keys.all(:order => 'active DESC, created_at DESC', :conditions => c.conditions)
-		respond_to do |format|
-			format.html # index.html.erb
-			format.json  { render :json => @gitolite_public_keys }
-		end
-	end
+	#def index
+	#	@status = if (session[:gitolite_public_key_filter_status]=params[:status]).nil?
+	#		GitolitePublicKey::STATUS_ACTIVE
+	#	elsif params[:status].blank?
+	#			nil
+	#	else 
+	#		params[:status] == "true"
+	#	end
+	#	c = ARCondition.new(!@status.nil? ? ["active=?", @status] : nil)
+	#
+	#	@gitolite_public_keys = @user.gitolite_public_keys.all(:order => 'active DESC, created_at DESC', :conditions => c.conditions)
+	#	respond_to do |format|
+	#		format.html # index.html.erb
+	#		format.json  { render :json => @gitolite_public_keys }
+	#	end
+	#end
 	
 	def edit
 		redirect_to url_for(:controller => 'my', :action => 'account')
@@ -35,12 +35,11 @@ class GitolitePublicKeysController < ApplicationController
 	def update
 		if @gitolite_public_key.update_attributes(params[:public_key])
 			flash[:notice] = l(:notice_public_key_updated)
-			redirect_to url_for(:action => 'index', :status => session[:gitolite_public_key_filter_status])
+			redirect_to url_for(:controller => 'my', :action => 'account')
 		else
 			render :action => 'edit'
 		end
 	end
-	
 	
 	def create
 		@gitolite_public_key = GitolitePublicKey.new(params[:public_key].merge(:user => @user))
@@ -52,12 +51,12 @@ class GitolitePublicKeysController < ApplicationController
 		redirect_to url_for(:controller => 'my', :action => 'account')
 	end
 	
-	def show
-		respond_to do |format|
-			format.html # show.html.erb
-			format.json { render :json => @gitolite_public_key }
-		end
-	end
+	#def show
+	#	respond_to do |format|
+	#		format.html # show.html.erb
+	#		format.json { render :json => @gitolite_public_key }
+	#	end
+	#end
 	
 	protected
 	
