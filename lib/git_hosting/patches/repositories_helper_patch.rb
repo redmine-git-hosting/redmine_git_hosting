58182d1ad508774675e259bfaa1a13a8f961be15
Eric <ericpaulbishop@gmail.com>
2011-04-24 14:02:27 -0400

    update english translation file

--- lib/git_hosting/patches/repositories_helper_patch.rb
+++ lib/git_hosting/patches/repositories_helper_patch.rb
@@ -3,13 +3,6 @@ module GitHosting
 	module Patches
 		module RepositoriesHelperPatch
 			def git_field_tags_with_hosting_configuration(form, repository)
-				git_daemon_label = "Git Daemon"
-				git_http_label = "Git Smart HTTP"
-				enabled_label = "Enabled"
-				disabled_label = "Disabled"
-				https_only_label = "HTTPS Only"
-				https_and_http_label = "HTTPS and HTTP"
-
 				gd = 1
 				gh = 1
 				if repository
@@ -20,20 +13,20 @@ module GitHosting
 				
 				gdd = gd == 0 ? " selected='selected' " : ""
 				gde = gd == 1 ? " selected='selected' " : ""
-				git_daemon_options = "<option #{gdd} value='0'>#{disabled_label}</option>" + (@project.is_public ? "<option #{gde} value='1'>#{enabled_label}</option>" : "") 
+				git_daemon_options = "<option #{gdd} value='0'>#{:label_disabled}</option>" + (@project.is_public ? "<option #{gde} value='1'>#{:label_enabled}</option>" : "") 
 				
 				
 				git_http_options = ""
 				hoption=0
-				name_order = [ disabled_label, https_only_label, https_and_http_label ]
+				name_order = [ :label_disabled, :label_https_only, :label_https_and_http ]
 				while hoption <= 2
 					selected = gh == hoption ? " selected='selected' " : ""
 					git_http_options = git_http_options + "<option value='#{hoption}' #{selected}>#{ name_order[hoption] }</option>"
 					hoption = hoption +1
 				end
 				
-				git_daemon_control = "<label for='git_daemon'>#{git_daemon_label}:</label><select id='repository_git_daemon' name='repository[git_daemon]'>" + git_daemon_options + "</select>";
-				git_http_control = "<label for='git_http'>#{git_http_label}:</label><select id='repository_git_http' name='repository[git_http]'>" + git_http_options + "</select>";
+				git_daemon_control = "<label for='git_daemon'>#{:label_git_daemon}:</label><select id='repository_git_daemon' name='repository[git_daemon]'>" + git_daemon_options + "</select>";
+				git_http_control = "<label for='git_http'>#{:label_git_http}:</label><select id='repository_git_http' name='repository[git_http]'>" + git_http_options + "</select>";
 
 				return "\n<p>" + git_daemon_control + "</p>\n<p>" +  git_http_control + "</p>\n"
 
