de7b9ffda5d7d487bbb27d87aa495d9be639f450
Eric Bishop <ericpaulbishop@gmail.com>
2011-07-19 12:58:21 -0400

    add git_cache migration

---
+++ db/migrate/20091119162428_create_git_cache.rb
@@ -0,0 +1,15 @@
+class CreateGitCache < ActiveRecord::Migration
+	def self.up
+		create_table :git_cache do |t|
+			t.column :command, :string
+			t.column :output, :text
+			t.column :used_count, :integer, :default =>1
+			t.timestamps
+			t.index :command 
+		end
+	end
+
+	def self.down
+		drop_table :git_cache
+	end
+end
