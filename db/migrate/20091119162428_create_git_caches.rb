826ce0bc481d7f2c4bed9190be5555b1ad917a59
Eric Bishop <ericpaulbishop@gmail.com>
2011-07-19 17:20:49 -0400

    git command can be really long, save as text not string

--- db/migrate/20091119162428_create_git_caches.rb
+++ db/migrate/20091119162428_create_git_caches.rb
@@ -1,7 +1,7 @@
 class CreateGitCaches < ActiveRecord::Migration
 	def self.up
 		create_table :git_caches do |t|
-			t.column :command, :string
+			t.column :command, :text
 			t.column :output, :text
 			t.column :used_count, :integer, :default =>1
 			t.timestamps
