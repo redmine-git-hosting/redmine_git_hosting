a3c457323373240ad991b5fa53ee5876b924f5a6
Eric <ericpaulbishop@gmail.com>
2011-07-24 22:30:41 -0400

    Revert "need to prefix get_tmp_dir with GitHosting too"
    This reverts commit 46e1d808787477acd53cbbc131452a3e5da6e708.

--- lib/git_hosting/hooks/git_adapter_hooks.rb
+++ lib/git_hosting/hooks/git_adapter_hooks.rb
@@ -8,7 +8,6 @@ module GitHosting
 			def self.check_hooks_installed
 				create_hooks_digests
 
-				local_dir = GitHosting.get_tmp_dir()
 				hook_dest_dir = File.join(local_dir, "gitolite-admin", "hooks", "common")
 				post_receive_hook_path = File.join(hook_dest_dir, 'post-receive')
 				post_receive_exists = %x[ test -r '#{post_receive_hook_path}' && echo 'yes' || echo 'no']
@@ -43,7 +42,7 @@ module GitHosting
 				
 				GitHosting.clone_or_pull_gitolite_admin
 				
-				local_dir = GitHosting.get_tmp_dir()
+				local_dir = get_tmp_dir()
 				hook_source_path = File.join(package_hooks_dir, hook_name)
 				hook_dest_dir = File.join(local_dir, "gitolite-admin", "hooks", "common")
 				hook_dest_path = File.join(hook_dest_dir,  hook_name.split('.')[0])
