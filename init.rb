e2e0024e0161831df493d13cff5764ecf30a235f
Eric Bishop <ericpaulbishop@gmail.com>
2011-08-16 13:36:28 -0400

    set accepts_nested_attributes_for and foreign key properly for repository extra

--- init.rb
+++ init.rb
@@ -60,7 +60,8 @@ end
 User.send(:has_many, :gitolite_public_keys, :dependent => :destroy)
 # initialize association from repository -> git hook keys
 Repository.send(:has_one, :hook_key, :class_name => 'GitHookKey', :dependent => :destroy)
-Repository.send(:has_one, :extra, :class_name => 'GitRepositoryExtra', :dependent => :destroy)
+Repository.send(:has_one, :extra, :foreign_key =>'repository_id', :class_name => 'GitRepositoryExtra', :dependent => :destroy)
+Repository.send(:accepts_nested_attributes_for, :extra, :allow_destroy =>true)  
 # initialize association from project -> repository mirrors
 Project.send(:has_many, :repository_mirrors, :dependent => :destroy)
 
