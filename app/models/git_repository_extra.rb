03ca7758d523148302655c4bb5bf75f16a256878
Eric Bishop <ericpaulbishop@gmail.com>
2011-08-17 18:09:39 -0400

    fix view for form fields other than git_daemon

--- app/models/git_repository_extra.rb
+++ app/models/git_repository_extra.rb
@@ -7,7 +7,6 @@ class GitRepositoryExtra < ActiveRecord::Base
 
 	belongs_to :repository, :class_name => 'Repository', :foreign_key => 'repository_id'
 	validates_associated :repository
-	validates_presence_of :repository_id, :key, :ivector, :git_http, :git_daemon, :notify_cia
 	attr_accessor :repository_id, :key, :ivector, :git_http, :git_daemon, :notify_cia
 
 	def after_initialize
