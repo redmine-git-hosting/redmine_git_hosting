0f9a02094fbf693a5c63ef6ce62c7c080908c386
Eric <ericpaulbishop@gmail.com>
2011-08-21 14:49:09 -0400

    more changes to repository controller patch, basically embed the old controller code,modified in it

--- lib/git_hosting/patches/repositories_controller_patch.rb
+++ lib/git_hosting/patches/repositories_controller_patch.rb
@@ -16,28 +16,39 @@ module GitHosting
 				if params[:repository_scm] == "Git"
 					repo_name= @project.parent ? File.join(GitHosting::get_full_parent_path(@project, true),@project.identifier) : @project.identifier
 					params[:repository][:url] = File.join(Setting.plugin_redmine_git_hosting['gitRepositoryBasePath'], "#{repo_name}.git")
-
-					# Update the git_repositories_extras table
-					# This needs to be done here because after some tries of adding,
-					# at runtime, accepts_nested_attributes_for, they all failed, and
-					# seemed too hacky.
-					if !@project.repository.nil?
-						@project.repository.extra.update_attributes(params[:extra])
-						@project.repository.extra.save
-					elsif !params[:extra].nil?
-						@project.repository = Repository.factory(params[:repository_scm])
-      						if !@project.repository.nil?
-							@project.repository.extra.update_attributes(params[:extra])
-						end
-					end
 				end
 				
-				edit_without_scm_settings
-				GitHosting.logger.debug "On edit_with_scm_settings after edit_without_scm_settings"
+				if params[:repository_scm] == "Git" || @project.repository.is_a?(Repository::Git)
+					#Evidently the ONLY way to update the repository.extra table is to basically copy/paste the existing controller code
+					#the update line needs to go in the dead center of it.				
+					@repository = @project.repository
+					if !@repository
+						@repository = Repository.factory(params[:repository_scm])
+						@repository.project = @project if @repository
+					end
+					if request.post? && @repository
+						@repository.attributes = params[:repository]
+						if !params[:extra].nil?
+							@project.repository.extra.update_attributes(params[:extra])
+						end
+						@repository.save
+					end
+					
+					GitHosting::update_repositories(@project, false) if !@project.repository.nil?
+					GitHosting::Hooks::GitAdapterHooks.setup_hooks(@project) if !@project.repository.nil?
 
-				GitHosting::update_repositories(@project, false) if !@project.repository.nil?
-				# Make sure the repository has updated hook settings
-				GitHosting::Hooks::GitAdapterHooks.setup_hooks(@project) if !@project.repository.nil?
+					render(:update) do |page|
+						page.replace_html "tab-content-repository", :partial => 'projects/settings/repository'
+						if @repository && !@project.repository
+							@project.reload #needed to reload association
+							page.replace_html "main-menu", render_main_menu(@project)
+						end
+					end
+	
+				else
+					edit_without_scm_settings
+				end
+				
 				
 			end
 
