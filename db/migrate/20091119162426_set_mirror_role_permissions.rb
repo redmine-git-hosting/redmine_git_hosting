19e86d21f11326451b9d3d2e5e97818ab83460c4
Eric Bishop <ericpaulbishop@gmail.com>
2011-08-22 15:55:05 -0400

    remove=>add, lots of typos today, I'm tired...

--- db/migrate/20091119162426_set_mirror_role_permissions.rb
+++ db/migrate/20091119162426_set_mirror_role_permissions.rb
@@ -4,9 +4,9 @@ class SetMirrorRolePermissions < ActiveRecord::Migration
 		GitHostingObserver.set_update_active(false)
 
 		manager_role   = Role.find_by_name("Manager")
-		manager_role.remove_permission! :view_repository_mirrors
-		manager_role.remove_permission! :edit_repository_mirrors
-		manager_role.remove_permission! :create_repository_mirrors
+		manager_role.add_permission! :view_repository_mirrors
+		manager_role.add_permission! :edit_repository_mirrors
+		manager_role.add_permission! :create_repository_mirrors
 		manager_role.save
 		
 		developer_role = Role.find_by_name("Developer")
