5416c4aa7c7ca29c8cddc45b84ea4737b27f84aa
Eric Bishop <ericpaulbishop@gmail.com>
2011-09-02 12:25:49 -0400

    set url and hook debug option as global settings, so we don't have to update all repos when they change.  The per-repo keys don't change when settings are saved, only when repo is first created, so we don't have to worry about them

--- lib/git_hosting/patches/git_hosting_settings_patch.rb
+++ lib/git_hosting/patches/git_hosting_settings_patch.rb
@@ -5,32 +5,13 @@ module GitHosting
 		module GitHostingSettingsPatch
 
 			def plugin_with_hook_settings_update
-				log_level = Setting.plugin_redmine_git_hosting['loggingLevel']
-				update_hooks = false
-				debug_hook = Setting.plugin_redmine_git_hosting['gitHooksDebug']
-				http_server = Setting.plugin_redmine_git_hosting['httpServer']
-
 
 				plugin_without_hook_settings_update
 
-				if params[:id] == 'redmine_git_hosting' and not params[:settings].nil?
-					if params[:settings][:updateAllHooks]=="yes" || debug_hook != Setting.plugin_redmine_git_hosting['gitHooksDebug'] || http_server != Setting.plugin_redmine_git_hosting['httpServer'] 
-						update_hooks = true
-						GitHosting.logger.info("Settings changed. Updating hook settings on ALL repositories")
-					end
-					new_logging_level = params[:settings][:loggingLevel]
-					if log_level != new_logging_level
-						GitHosting.logger.info "Changing logging level from #{log_level} to #{new_logging_level}"
-						Setting.plugin_redmine_git_hosting['loggingLevel'] = new_logging_level
-						GitHosting.logger.level = new_logging_level.to_i
-					end
-				end
-				if update_hooks
-					t = Thread.new do
-						# Let's sleep a while to allow the db settings to be saved
-						sleep 1.5
-						GitHosting::Hooks::GitAdapterHooks.setup_hooks()
-					end
+				t = Thread.new do
+					# Let's sleep a while to allow the db settings to be saved
+					sleep 1.5
+					GitHosting::Hooks::GitAdapterHooks.update_hook_url_and_debug()
 				end
 			end
 
