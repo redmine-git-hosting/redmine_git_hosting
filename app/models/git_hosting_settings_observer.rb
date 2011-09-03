3d59a3e3b6bb413189ee410b110e86bacfafe5cc
Eric <ericpaulbishop@gmail.com>
2011-09-03 12:56:23 -0400

    add code for git_hosting_settings_observer

---
+++ app/models/git_hosting_settings_observer.rb
@@ -0,0 +1,22 @@
+class GitHostingSettingsObserver < ActiveRecord::Observer
+	observe :settings
+
+	@@old_hook_debug = nil
+	@@old_http_server = nil
+	@@old_git_user = nil
+
+	def reload_this_observer
+		observed_classes.each do |klass|
+			klass.name.constantize.add_observer(self)
+		end
+	end
+
+
+
+	def before_save
+
+	end
+
+	def after_save
+	end
+end
--- init.rb
+++ init.rb
@@ -70,8 +70,13 @@ Project.send(:has_many, :repository_mirrors, :dependent => :destroy)
 config.after_initialize do
 	if config.action_controller.perform_caching
 		ActiveRecord::Base.observers = ActiveRecord::Base.observers << GitHostingObserver
+		ActiveRecord::Base.observers = ActiveRecord::Base.observers << GitHostingSettingsObserver
+		
 		ActionController::Dispatcher.to_prepare(:git_hosting_observer_reload) do
 			GitHostingObserver.instance.reload_this_observer
+		end	
+		ActionController::Dispatcher.to_prepare(:git_hosting_settings_observer_reload) do
+			GitHostingSettingsObserver.instance.reload_this_observer
 		end	
 	end
 end
