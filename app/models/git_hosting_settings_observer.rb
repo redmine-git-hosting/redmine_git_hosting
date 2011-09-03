75768e81754408f0c51f07d16101c49ee4db72c7
Eric <ericpaulbishop@gmail.com>
2011-09-03 14:10:57 -0400

    use observer to update GitHostingSettings, not a patch to controller -- it's a bit cleaner this way

--- app/models/git_hosting_settings_observer.rb
+++ app/models/git_hosting_settings_observer.rb
@@ -13,10 +13,34 @@ class GitHostingSettingsObserver < ActiveRecord::Observer
 
 
 
-	def before_save
-
+	def before_save(object)
+		if object.name == "plugin_redmine_git_hosting"
+			update_cached_vars
+		end
 	end
 
-	def after_save
+	def after_save(object)
+		if object.name == "plugin_redmine_git_hosting"
+			if @@old_git_user != Setting.plugin_redmine_git_hosting['gitUser'] 
+				
+				%x[ rm -rf '#{ GitHosting.get_tmp_dir }' ]
+				GitHosting::Hooks::GitAdapterHooks.setup_hooks
+				GitHosting.update_repositories( Project.find(:all), false)
+
+			elsif @@old_http_server !=  Setting.plugin_redmine_git_hosting['httpServer'] || @@old_http_server =  Setting.plugin_redmine_git_hosting['httpServer']
+
+				GitHosting::Hooks::GitAdapterHooks.update_hook_url_and_debug
+			
+			end
+			update_cached_vars
+		end
+	end
+
+	private
+
+	def update_cached_vars
+		@@old_hook_debug = Setting.plugin_redmine_git_hosting['gitHooksDebug']
+		@@old_http_server =  Setting.plugin_redmine_git_hosting['httpServer']
+		@@old_git_user =  Setting.plugin_redmine_git_hosting['gitUser']
 	end
 end
