26efe27f772ec8a56ee029d11de9b6d6aa436408
Eric Bishop <ericpaulbishop@gmail.com>
2011-09-06 11:26:38 -0400

    fix issue with internationalization when setting up role permissions during migration

--- db/migrate/20091119162426_set_mirror_role_permissions.rb
+++ db/migrate/20091119162426_set_mirror_role_permissions.rb
@@ -3,13 +3,13 @@ class SetMirrorRolePermissions < ActiveRecord::Migration
 		
 		GitHostingObserver.set_update_active(false)
 
-		manager_role   = Role.find_by_name("Manager")
+		manager_role   = Role.find_by_name(I18n.t(:default_role_manager))
 		manager_role.add_permission! :view_repository_mirrors
 		manager_role.add_permission! :edit_repository_mirrors
 		manager_role.add_permission! :create_repository_mirrors
 		manager_role.save
 		
-		developer_role = Role.find_by_name("Developer")
+		developer_role = Role.find_by_name(I18n.t(:default_role_developer))
 		developer_role.add_permission! :view_repository_mirrors
 		developer_role.save		
 		
