c20f02f1d3b52d1ac49a6b3aebcab11825a8acfa
Eric Bishop <ericpaulbishop@gmail.com>
2011-09-06 17:26:32 -0400

    manager=>developer in SetMirrorRolePermeissions.down

--- db/migrate/20091119162426_set_mirror_role_permissions.rb
+++ db/migrate/20091119162426_set_mirror_role_permissions.rb
@@ -29,7 +29,7 @@ class SetMirrorRolePermissions < ActiveRecord::Migration
 			manager_role.remove_permission! :create_repository_mirrors
 			manager_role.save
 		
-			developer_role = Role.find_by_name(I18n.t(:default_role_manager))
+			developer_role = Role.find_by_name(I18n.t(:default_role_developer))
 			developer_role.remove_permission! :view_repository_mirrors
 			developer_role.save
 		rescue
