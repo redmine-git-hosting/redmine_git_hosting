65cf26236731c8741057dbc7777c94ad34899b82
Eric Bishop <ericpaulbishop@gmail.com>
2011-09-06 17:24:29 -0400

    rescue Role migration errors, as evidently, some people don't have Manager/Developer Roles set at all....

--- db/migrate/20091119162426_set_mirror_role_permissions.rb
+++ db/migrate/20091119162426_set_mirror_role_permissions.rb
@@ -1,32 +1,38 @@
 class SetMirrorRolePermissions < ActiveRecord::Migration
 	def self.up
-		
-		GitHostingObserver.set_update_active(false)
+	
+		begin	
+			GitHostingObserver.set_update_active(false)
 
-		manager_role   = Role.find_by_name(I18n.t(:default_role_manager))
-		manager_role.add_permission! :view_repository_mirrors
-		manager_role.add_permission! :edit_repository_mirrors
-		manager_role.add_permission! :create_repository_mirrors
-		manager_role.save
+			manager_role   = Role.find_by_name(I18n.t(:default_role_manager))
+			manager_role.add_permission! :view_repository_mirrors
+			manager_role.add_permission! :edit_repository_mirrors
+			manager_role.add_permission! :create_repository_mirrors
+			manager_role.save
 		
-		developer_role = Role.find_by_name(I18n.t(:default_role_developer))
-		developer_role.add_permission! :view_repository_mirrors
-		developer_role.save		
+			developer_role = Role.find_by_name(I18n.t(:default_role_developer))
+			developer_role.add_permission! :view_repository_mirrors
+			developer_role.save
+		rescue
+		end
 		
 	end
 
 	def self.down
 		
-		GitHostingObserver.set_update_active(false)
+		begin
+			GitHostingObserver.set_update_active(false)
 	
-		manager_role   = Role.find_by_name("Manager")
-		manager_role.remove_permission! :view_repository_mirrors
-		manager_role.remove_permission! :edit_repository_mirrors
-		manager_role.remove_permission! :create_repository_mirrors
-		manager_role.save
+			manager_role   = Role.find_by_name(I18n.t(:default_role_manager))
+			manager_role.remove_permission! :view_repository_mirrors
+			manager_role.remove_permission! :edit_repository_mirrors
+			manager_role.remove_permission! :create_repository_mirrors
+			manager_role.save
 		
-		developer_role = Role.find_by_name("Developer")
-		developer_role.remove_permission! :view_repository_mirrors
-		developer_role.save	
+			developer_role = Role.find_by_name(I18n.t(:default_role_manager))
+			developer_role.remove_permission! :view_repository_mirrors
+			developer_role.save
+		rescue
+		end
 	end
 end
