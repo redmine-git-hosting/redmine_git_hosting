f49b717ba4a6fc76367288d48eec90e2cec5233d
Eric <ericpaulbishop@gmail.com>
2011-09-18 13:48:36 -0400

    soem fixes for zero clipboard for mirror pubkey

--- assets/javascripts/zero_clipboard_setup.js
+++ assets/javascripts/zero_clipboard_setup.js
@@ -1,6 +1,7 @@
 var zero_clipboard_source_input_control_id = "git_url_text";
 
-document.observe("dom:loaded", function() {
+function reset_zero_clipboard()
+{
 	var clip_container = $('clipboard_container');
 	if (clip_container) {
 		clip_container.show();
@@ -14,11 +15,11 @@ document.observe("dom:loaded", function() {
 			clipboard.setText($(zero_clipboard_source_input_control_id).value);
 		});
 	}
-});
+}
 
 function setZeroClipboardInputSource(id)
 {
 	zero_clipboard_source_input_control_id = id;
 }
 
-
+document.observe("dom:loaded", function() { reset_zero_clipboard(); } )
