548eb504712411cb7ce9e7289962bf4f78b1138f
Eric <ericpaulbishop@gmail.com>
2011-09-18 16:26:14 -0400

    some fixes so our hackish solution doesn't cause any issues that are too egregious

--- assets/javascripts/zero_clipboard_setup.js
+++ assets/javascripts/zero_clipboard_setup.js
@@ -1,10 +1,22 @@
 var zero_clipboard_source_input_control_id = "git_url_text";
+var clipboard = null
 
 function reset_zero_clipboard()
 {
 	var clip_container = $('clipboard_container');
 	if (clip_container) {
 		clip_container.show();
+
+		var cur_children = clip_container.childNodes;
+		var ci;
+		for(ci=0; ci< cur_children.length; ci++)
+		{
+			var c = cur_children[ci];
+			if(c.id != "clipboard_button")
+			{
+				clip_container.removeChild(c);
+			}
+		}
 
 		clipboard = new ZeroClipboard.Client();
 
