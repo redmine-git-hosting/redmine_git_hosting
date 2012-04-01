f2c3b7f0bcd68e37bd7ccf71bcd6bffa97b3bca2
John Kubiatowicz <kubitron@cs.berkeley.edu>
2012-04-01 15:02:17 -0700

    Extra protection when defining backward-compatible versions of labelled_form_for and labelled_remote_form_for: do not do so if lib/tabular_form_builder.rb undefined!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,13 @@ module ApplicationHelper
 	# Only define these if not already defined (although load-order seems to load plugin
 	# helpers before main ones, so check not necessary).
 	#
-	# 1/05/12
-	# John Kubiatowicz
-	if !defined?(labelled_form_for)
+	# 1/05/12 John Kubiatowicz
+  	# 4/01/12 Only define backward-compatible functions if TabularFormBuilder exists
+  	#	  (seems some versions of rails will go ahead and define these functions
+	# 	  and override properly defined versions).  Note that Redmine 1.4+ removes
+  	# 	  lib/tabular_form_builder.rb but defines these functions using new
+	#	  builder functions...
+	if !defined?(labelled_form_for) && File.exists?(Rails.root.join("lib/tabular_form_builder.rb"))
     		def labelled_form_for(*args, &proc)
                 	args << {} unless args.last.is_a?(Hash)
  			options = args.last
@@ -16,7 +20,7 @@ module ApplicationHelper
                 end
     	end
 
-	if !defined?(labelled_remote_form_for)
+	if !defined?(labelled_remote_form_for) && File.exists?(Rails.root.join("lib/tabular_form_builder.rb"))
 		def labelled_remote_form_for(*args, &proc)
 			args << {} unless args.last.is_a?(Hash)
  			options = args.last
