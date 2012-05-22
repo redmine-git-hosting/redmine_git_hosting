419c5b4a9bd214678d792e2f95d24b6b44a98179
Greg Thornton <xdissent@me.com>
2012-05-22 01:57:30 -0500

    Adding post-receive URL modes

--- app/models/repository_post_receive_url.rb
+++ app/models/repository_post_receive_url.rb
@@ -12,6 +12,14 @@ class RepositoryPostReceiveUrl < ActiveRecord::Base
   named_scope :active, {:conditions => {:active => RepositoryPostReceiveUrl::STATUS_ACTIVE}}
   named_scope :inactive, {:conditions => {:active => RepositoryPostReceiveUrl::STATUS_INACTIVE}}
 
+  validates_inclusion_of :mode, :in => [:github, :get]
+  def mode
+    read_attribute(:mode).to_sym rescue nil
+  end
+  def mode= (value)
+    write_attribute(:mode, (value.to_sym && value.to_sym.to_s rescue nil))
+  end
+
   def to_s
     return File.join("#{project.identifier}-#{url}")
   end
