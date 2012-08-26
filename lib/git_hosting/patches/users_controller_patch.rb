d512172064161524d18103e48002a5abfc28a07f
John Kubiatowicz <kubitron@cs.berkeley.edu>
2012-08-26 14:25:48 -0700

    Fixed non-standard condition that tripped-up SQLite3.

--- lib/git_hosting/patches/users_controller_patch.rb
+++ lib/git_hosting/patches/users_controller_patch.rb
@@ -61,8 +61,8 @@ module GitHosting
 
 	    # Add in values for viewing public keys:
 	    def set_public_key_values
-		@gitolite_user_keys = @user.gitolite_public_keys.all(:order => 'title ASC, created_at ASC', :conditions => "active=1 && key_type=#{GitolitePublicKey::KEY_TYPE_USER}")
-		@gitolite_deploy_keys = @user.gitolite_public_keys.all(:order => 'title ASC, created_at ASC', :conditions => "active=1 && key_type=#{GitolitePublicKey::KEY_TYPE_DEPLOY}")
+		@gitolite_user_keys = @user.gitolite_public_keys.active.user_key.find(:all,:order => 'title ASC, created_at ASC')
+		@gitolite_deploy_keys = @user.gitolite_public_keys.active.deploy_key.find(:all,:order => 'title ASC, created_at ASC')
 		@gitolite_public_keys = @gitolite_user_keys + @gitolite_deploy_keys
 		@gitolite_public_key = @gitolite_public_keys.detect{|x| x.id == params[:public_key_id].to_i}
 		if @gitolite_public_key.nil?
