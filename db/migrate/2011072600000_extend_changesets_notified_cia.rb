27f9bb8e931033766cf48dcbc5df5be9e1f41a47
crocket <crockabiscuit@gmail.com>
2012-11-21 23:40:29 +0900

    Fix an error that occurs while uninstalling from DB
    2011072600000_extend_changesets_notified_cia.rb tries to delete
    notified_cia column in changesets table, but
    2011081700000_move_notified_cia_to_git_cia_notifications.rb deletes
    notified_cia column before it does, so an error occurs.
    
    A solution is to let 2011072600000_extend_changesets_notified_cia.rb
    remove notified_cia column only if the column exists.

--- db/migrate/2011072600000_extend_changesets_notified_cia.rb
+++ db/migrate/2011072600000_extend_changesets_notified_cia.rb
@@ -5,6 +5,10 @@ class ExtendChangesetsNotifiedCia < ActiveRecord::Migration
 
     def self.down
 	# Deal with fact that one of next migrations doesn't restore :notified_cia
-	remove_column(:changesets, :notified_cia) rescue nil
+	remove_column :changesets, :notified_cia if self.column_exists?(:changesets, :notified_cia)
+    end
+
+    def self.column_exists?(table_name, column_name)
+	columns(table_name).any?{ |c| c.name == column_name.to_s }
     end
 end
