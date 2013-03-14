5247ec9d20d67fed59ebf7cab62cffecc48f1edf
Nicolas <nrodriguez@jbox-web.com>
2013-03-14 03:38:44 +0100

    Rename application helper + Rails 3 fixes

---
+++ app/helpers/application_ext_helper.rb
@@ -0,0 +1,49 @@
+module ApplicationExtHelper
+
+  # These functions are for backward compatability with versions of redmine < 1.3
+  # Only define these if not already defined (although load-order seems to load plugin
+  # helpers before main ones, so check not necessary).
+  #
+  # 1/05/12 John Kubiatowicz
+  # 4/01/12 Only define backward-compatible functions if TabularFormBuilder exists
+  #   (seems some versions of rails will go ahead and define these functions
+  #   and override properly defined versions).  Note that Redmine 1.4+ removes
+  #   lib/tabular_form_builder.rb but defines these functions using new
+  #   builder functions...
+  # 8/10/12 Added "labelled_fields_for", since that seems to be coming in 1.4
+  if !defined?(labelled_form_for) && File.exists?(Rails.root.join("lib/tabular_form_builder.rb"))
+    def labelled_form_for(*args, &proc)
+      args << {} unless args.last.is_a?(Hash)
+      options = args.last
+      options.merge!({:builder => Redmine::Views::LabelledFormBuilder, :lang => current_language})
+      form_for(*args, &proc)
+    end
+  end
+
+  if !defined?(labelled_remote_form_for) && File.exists?(Rails.root.join("lib/tabular_form_builder.rb"))
+    def labelled_remote_form_for(*args, &proc)
+      args << {} unless args.last.is_a?(Hash)
+      options = args.last
+      options.merge!({:builder => Redmine::Views::LabelledFormBuilder, :lang => current_language, :remote => true})
+      form_for(*args, &proc)
+    end
+  end
+
+  if !defined?(labelled_fields_for) && File.exists?(Rails.root.join("lib/tabular_form_builder.rb"))
+    def labelled_fields_for(*args, &proc)
+      args << {} unless args.last.is_a?(Hash)
+      options = args.last
+      options.merge!({:builder => Redmine::Views::LabelledFormBuilder, :lang => current_language})
+      fields_for(*args, &proc)
+    end
+  end
+
+  # Generic helper functions
+  def reldir_add_dotslash(path)
+    # Is this a relative path?
+    stripped = (path || "").lstrip.rstrip
+    norm = File.expand_path(stripped,"/")
+    ((stripped[0,1] != "/")?".":"") + norm + ((norm[-1,1] != "/")?"/":"")
+  end
+
+end
--- app/helpers/git_hosting_helper.rb
+++ app/helpers/git_hosting_helper.rb
@@ -3,7 +3,7 @@ require "net/http"
 
 module GitHostingHelper
 
-
+  include Redmine::I18n
 
   def self.git_daemon_enabled(repository, value)
     gd = 1
@@ -77,6 +77,44 @@ module GitHostingHelper
       return true if role.allowed_to?(:create_deployment_keys)
     end
     false
+  end
+
+  # Port-receive Mode
+  def self.post_receive_mode(prurl)
+    if prurl.active == 0
+      l(:label_inactive)
+    elsif prurl.mode == :github
+      l(:label_github_post)
+    else
+      l(:label_empty_get)
+    end
+  end
+
+  # Refspec for mirrors
+  def self.refspec(mirror, max_refspec=0)
+    if mirror.push_mode == RepositoryMirror::PUSHMODE_MIRROR
+      l(:all_references)
+    else
+      result = []
+      result << l(:all_branches) if mirror.include_all_branches
+      result << l(:all_tags) if mirror.include_all_tags
+      result << mirror.explicit_refspec if (max_refspec == 0) || ((1..max_refspec) === mirror.explicit_refspec.length)
+      result << l(:explicit) if (max_refspec > 0) && (mirror.explicit_refspec.length > max_refspec)
+      result.join(",<br />")
+    end
+  end
+
+  # Mirror Mode
+  def self.mirror_mode(mirror)
+    if mirror.active == 0
+      l(:label_inactive)
+    else
+      [l(:label_mirror), l(:label_forced), l(:label_unforced)][mirror.push_mode]
+    end
+  end
+
+  def self.plugin_asset_link(asset_name)
+    File.join(Redmine::Utils.relative_url_root, 'plugin_assets', 'redmine_git_hosting', 'images', asset_name)
   end
 
   def url_for_revision(revision)
