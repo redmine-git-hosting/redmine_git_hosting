4d553bbcd1f9a8801839fd3c89cb04e2d82f25eb
Nicolas <nrodriguez@jbox-web.com>
2013-03-14 03:35:59 +0100

    Re indent helpers

--- app/helpers/sys_helper.rb
+++ app/helpers/sys_helper.rb
@@ -1,3 +1,2 @@
-# Added blank help for Rails 1.9.3+
 module SysHelper
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,22 +2,23 @@ require_dependency 'users_helper'
 
 # Add a tab for editing public keys to the user's page....
 module GitHostingUsersHelperPatch
-    def self.included(base)
-	base.send(:include, InstanceMethods)
-	base.class_eval do
-	    alias_method_chain :user_settings_tabs, :public_keys
-	end
-    end
 
-    module InstanceMethods
-	# Add a public_keys tab to the user administration page
-	def user_settings_tabs_with_public_keys
-	    tabs = user_settings_tabs_without_public_keys
-	    tabs << { :name => 'keys', :partial => 'gitolite_public_keys/view', :label => :label_public_keys }
-	    return tabs
-	end
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+      alias_method_chain :user_settings_tabs, :public_keys
     end
+  end
+
+  module InstanceMethods
+    # Add a public_keys tab to the user administration page
+    def user_settings_tabs_with_public_keys
+      tabs = user_settings_tabs_without_public_keys
+      tabs << { :name => 'keys', :partial => 'gitolite_public_keys/view', :label => :label_public_keys }
+      return tabs
+    end
+  end
+
 end
-
 
 UsersHelper.send(:include, GitHostingUsersHelperPatch)
