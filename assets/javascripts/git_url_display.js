3b1b983f15030495dc190fcc7b62bec31ad3a40d
root <root@homeserver>
2013-04-02 08:11:44 +0200

    Fixed ZeroClipboard in Redmine 2.x

--- assets/javascripts/git_url_display.js
+++ assets/javascripts/git_url_display.js
@@ -1,51 +1,44 @@
-var allGitUrlIds = ["git_url_ssh", "git_url_http", "git_url_git"]
-function updateGitUrl(el)
-{
-	guHttpBase = guHttpBase.replace(/\/$/, "")
+var allGitUrlIds = ["git_url_ssh", "git_url_http", "git_url_git"];
 
-	var urls=[]
-	var gitSHP = /:\d+$/.test(guGitServer)
-	urls["git_url_ssh"]  = [(gitSHP ? "ssh://" : "") + guGitUser + "@" + guGitServer + (gitSHP ? "/" : ":") + guSshURL, guUserIsCommitter]
-	urls["git_url_http"] = [guHttpProto + "://" + ( (!guProjectIsPublic) || guUserIsCommitter ? encodeURIComponent(guUser) + "@" : "") + guHttpBase + "/" + guHttpURL, guUserIsCommitter]
-	urls["git_url_git"]  = ["git://" + guGitServer + "/" + guSshURL, false]
-	var allGitUrlIds = ["git_url_ssh", "git_url_http", "git_url_git"]
+function updateGitUrl(el) {
+  guHttpBase = guHttpBase.replace(/\/$/, "");
 
-	var selected_id = el.id
-	document.getElementById("git_url_text").value = urls[selected_id][0];
-	document.getElementById("git_url_access").innerHTML = urls[selected_id][1] ? "Read+Write" : "Read-Only"
+  var urls = [];
+  var gitSHP = /:\d+$/.test(guGitServer);
 
-	var i
-	for(i=0;i<allGitUrlIds.length; i++)
-	{
-		var test_id = allGitUrlIds[i];
-		var test_el = document.getElementById(test_id)
-		if (test_el != null)
-		{
-			test_el.className = test_id == selected_id ? "selected" : ""
-		}
-	}
+  urls["git_url_ssh"]  = [(gitSHP ? "ssh://" : "") + guGitUser + "@" + guGitServer + (gitSHP ? "/" : ":") + guSshURL, guUserIsCommitter];
+  urls["git_url_http"] = [guHttpProto + "://" + ( (!guProjectIsPublic) || guUserIsCommitter ? encodeURIComponent(guUser) + "@" : "") + guHttpBase + "/" + guHttpURL, guUserIsCommitter];
+  urls["git_url_git"]  = ["git://" + guGitServer + "/" + guSshURL, false];
+
+  var selected_id = el.id;
+  document.getElementById('git_url_text').value = urls[selected_id][0];
+  document.getElementById('git_url_access').innerHTML = urls[selected_id][1] ? "Read+Write" : "Read-Only";
+
+  var i;
+  for(i=0; i<allGitUrlIds.length; i++) {
+    var test_id = allGitUrlIds[i];
+    var test_el = document.getElementById(test_id);
+    if (test_el != null) {
+      test_el.className = test_id == selected_id ? "selected" : "";
+    }
+  }
 }
 
-function setGitUrlOnload()
-{
-	// highlight input text on click
-	var git_input = document.getElementById("git_url_text");
-	if (git_input) {
-		git_input.setAttribute("onclick", "this.select()");
-	}
+function setGitUrlOnload() {
+  // highlight input text on click
+  var git_input = document.getElementById('git_url_text');
+  if (git_input) {
+    git_input.setAttribute("onclick", "this.select()");
+  }
 
-	var i
-	var firstEl = null
-	for(i=0;i<allGitUrlIds.length; i++)
-	{
-		var el = document.getElementById(allGitUrlIds[i]);
-		if(el != null)
-		{
-			firstEl = firstEl == null ? el : firstEl
-			el.setAttribute("onclick", "updateGitUrl(this)")
-		}
-	}
-	updateGitUrl(firstEl)
-
+  var i;
+  var firstEl = null;
+  for(i=0; i<allGitUrlIds.length; i++) {
+    var el = document.getElementById(allGitUrlIds[i]);
+    if(el != null) {
+      firstEl = firstEl == null ? el : firstEl;
+      el.setAttribute("onclick", "updateGitUrl(this)");
+    }
+  }
+  updateGitUrl(firstEl);
 }
-
--- assets/javascripts/zero_clipboard_setup.js
+++ assets/javascripts/zero_clipboard_setup.js
@@ -1,38 +1,29 @@
-var zero_clipboard_source_input_control_id = "git_url_text";
-var clipboard = null
+jQuery.noConflict();
 
-function reset_zero_clipboard()
-{
-	var clip_container = $('clipboard_container');
-	if (clip_container) {
-		clip_container.show();
-		clip_container.style.fontFamily="serif"
+var clipboard = null;
 
-		var cur_children = clip_container.childNodes;
-		var ci;
-		for(ci=0; ci< cur_children.length; ci++)
-		{
-			var c = cur_children[ci];
-			if(c.id != "clipboard_button")
-			{
-				clip_container.removeChild(c);
-			}
-		}
+function reset_zero_clipboard() {
+  var clip_container = jQuery('#clipboard_container');
+  if (clip_container) {
+    clip_container.show();
 
-		clipboard = new ZeroClipboard.Client();
+    //~ var cur_children = clip_container.childNodes;
+    //~ var ci;
+    //~ for(ci=0; ci< cur_children.length; ci++) {
+      //~ var c = cur_children[ci];
+      //~ if(c.id != "clipboard_button") {
+        //~ clip_container.removeChild(c);
+      //~ }
+    //~ }
 
-		clipboard.setHandCursor(true);
-		clipboard.glue('clipboard_button', 'clipboard_container');
-
-		clipboard.addEventListener('mouseOver', function (client) {
-			clipboard.setText($(zero_clipboard_source_input_control_id).value);
-		});
-	}
+    clipboard = new ZeroClipboard.Client();
+    clipboard.glue('clipboard_button', 'clipboard_container');
+    clipboard.addEventListener('mouseOver', function (client) {
+      clipboard.setText(document.getElementById('git_url_text').value);
+    });
+  }
 }
 
-function setZeroClipboardInputSource(id)
-{
-	zero_clipboard_source_input_control_id = id;
-}
-
-document.observe("dom:loaded", function() { reset_zero_clipboard(); } )
+jQuery(document).ready(function() {
+  reset_zero_clipboard();
+});
