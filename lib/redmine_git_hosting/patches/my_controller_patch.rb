dfc62835403f689387b78a363ac75d6554b15641
Nicolas <nrodriguez@jbox-web.com>
2013-09-14 04:39:47 +0200

    Don't be a block blocker

--- lib/redmine_git_hosting/patches/my_controller_patch.rb
+++ lib/redmine_git_hosting/patches/my_controller_patch.rb
@@ -3,35 +3,40 @@ module RedmineGitHosting
     module MyControllerPatch
 
       def self.included(base)
+        base.send(:include, InstanceMethods)
         base.class_eval do
           unloadable
-          helper :gitolite_public_keys
-        end
 
-        begin
-          base.send(:alias_method_chain, :account, :public_keys)
-        rescue
+          alias_method_chain :account, :git_hosting
+
+          helper :gitolite_public_keys
+          include GitolitePublicKeysHelper
         end
       end
 
-      # Add in values for viewing public keys:
-      def account_with_public_keys
-        # Previous routine
-        account_without_public_keys
+      module InstanceMethods
 
-        @gitolite_user_keys = @user.gitolite_public_keys.active.user_key.find(:all,:order => 'title ASC, created_at ASC')
-        @gitolite_deploy_keys = @user.gitolite_public_keys.active.deploy_key.find(:all,:order => 'title ASC, created_at ASC')
-        @gitolite_public_keys = @gitolite_user_keys + @gitolite_deploy_keys
-        @gitolite_public_key = @gitolite_public_keys.detect{|x| x.id == params[:public_key_id].to_i}
-        if @gitolite_public_key.nil?
-          if params[:public_key_id]
-            # public_key specified that doesn't belong to @user.  Kill off public_key_id and try again
-            redirect_to :public_key_id => nil, :tab => nil
-            return
-          else
-            @gitolite_public_key = GitolitePublicKey.new
+        # Add in values for viewing public keys:
+        def account_with_git_hosting(&block)
+          # Previous routine
+          account_without_git_hosting(&block)
+
+          @gitolite_user_keys   = @user.gitolite_public_keys.active.user_key.find(:all,:order => 'title ASC, created_at ASC')
+          @gitolite_deploy_keys = @user.gitolite_public_keys.active.deploy_key.find(:all,:order => 'title ASC, created_at ASC')
+          @gitolite_public_keys = @gitolite_user_keys + @gitolite_deploy_keys
+          @gitolite_public_key  = @gitolite_public_keys.detect{|x| x.id == params[:public_key_id].to_i}
+
+          if @gitolite_public_key.nil?
+            if params[:public_key_id]
+              # public_key specified that doesn't belong to @user.  Kill off public_key_id and try again
+              redirect_to :public_key_id => nil, :tab => nil
+              return
+            else
+              @gitolite_public_key = GitolitePublicKey.new
+            end
           end
         end
+
       end
 
     end
