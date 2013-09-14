3d946c308075c98a6a23af4c37e58e31da4d2998
Nicolas <nrodriguez@jbox-web.com>
2013-09-14 20:29:28 +0200

    Load InstanceMethods properly

--- lib/redmine_git_hosting/patches/roles_controller_patch.rb
+++ lib/redmine_git_hosting/patches/roles_controller_patch.rb
@@ -3,86 +3,88 @@ module RedmineGitHosting
     module RolesControllerPatch
 
       def self.included(base)
+        base.send(:include, InstanceMethods)
         base.class_eval do
           unloadable
+
+          begin
+            # RESTfull (post-1.4)
+            alias_method_chain :create,   :git_hosting
+          rescue
+            # Not RESTfull (pre-1.4)
+            alias_method_chain :new,      :git_hosting rescue nil
+          end
+
+          begin
+            # RESTfull (post-1.4)
+            alias_method_chain :update,   :git_hosting
+          rescue
+            # Not RESTfull (pre-1.4)
+            alias_method_chain :edit,     :git_hosting rescue nil
+          end
+
+          alias_method_chain :destroy,     :git_hosting rescue nil
+          alias_method_chain :permissions, :git_hosting rescue nil
+        end
+      end
+
+
+      module InstanceMethods
+
+        # Pre-1.4 (Not RESTfull)
+        def new_with_git_hosting(&block)
+          GitHostingObserver.set_update_active(false)
+          new_without_git_hosting(&block)
+          GitHostingObserver.set_update_active(true)
         end
 
-        begin
-          # RESTfull (post-1.4)
-          base.send(:alias_method_chain, :create, :disable_update)
-        rescue
-          # Not RESTfull (pre-1.4)
-          base.send(:alias_method_chain, :new, :disable_update) rescue nil
+        # Post-1.4 (RESTfull)
+        def create_with_git_hosting(&block)
+          GitHostingObserver.set_update_active(false)
+          create_without_git_hosting(&block)
+          GitHostingObserver.set_update_active(true)
         end
 
-        begin
-          # RESTfull (post-1.4)
-          base.send(:alias_method_chain, :update, :disable_update)
-        rescue
-          # Not RESTfull (pre-1.4)
-          base.send(:alias_method_chain, :edit, :disable_update) rescue nil
+        # Pre-1.4 (Not RESTfull)
+        def edit_with_git_hosting(&block)
+          GitHostingObserver.set_update_active(false)
+          edit_without_git_hosting(&block)
+          GitHostingObserver.set_update_active(true)
         end
 
-        base.send(:alias_method_chain, :destroy, :disable_update) rescue nil
-      end
+        # Post-1.4 (RESTfull)
+        def update_with_git_hosting(&block)
+          GitHostingObserver.set_update_active(false)
+          update_without_git_hosting(&block)
+          GitHostingObserver.set_update_active(true)
+        end
 
-      # Pre-1.4 (Not RESTfull)
-      def new_with_disable_update
-        # Turn of updates during repository update
-        GitHostingObserver.set_update_active(false);
+        def permissions_with_git_hosting(&block)
+          GitHostingObserver.set_update_active(false)
+          permissions_without_git_hosting(&block)
+          GitHostingObserver.set_update_active(true)
 
-        # Do actual update
-        new_without_disable_update
+          #if request.post?
+          #  resync
+          #end
+        end
 
-        # Reenable updates to perform a single update
-        GitHostingObserver.set_update_active(true);
-      end
+        def destroy_with_git_hosting(&block)
+          GitHostingObserver.set_update_active(false)
+          destroy_without_git_hosting(&block)
+          GitHostingObserver.set_update_active(true)
+        end
 
-      # Post-1.4 (RESTfull)
-      def create_with_disable_update
-        # Turn of updates during repository update
-        GitHostingObserver.set_update_active(false);
+        private
 
-        # Do actual update
-        create_without_disable_update
+        def resync
+          projects = Project.active_or_archived.find(:all, :include => :repositories)
+          if projects.length > 0
+            GitHosting.logger.info "Role has been modified, resync all projects..."
+            GithostingShellWorker.perform_async({ :command => :update_all_projects, :object => projects.length })
+          end
+        end
 
-        # Reenable updates to perform a single update
-        GitHostingObserver.set_update_active(true);
-      end
-
-      # Pre-1.4 (Not RESTfull)
-      def edit_with_disable_update
-        # Turn of updates during repository update
-        GitHostingObserver.set_update_active(false);
-
-        # Do actual update
-        edit_without_disable_update
-
-        # Reenable updates to perform a single update
-        GitHostingObserver.set_update_active(true);
-      end
-
-      # Post-1.4 (RESTfull)
-      def update_with_disable_update
-        # Turn of updates during repository update
-        GitHostingObserver.set_update_active(false);
-
-        # Do actual update
-        update_without_disable_update
-
-        # Reenable updates to perform a single update
-        GitHostingObserver.set_update_active(true);
-      end
-
-      def destroy_with_disable_update
-        # Turn of updates during repository update
-        GitHostingObserver.set_update_active(false);
-
-        # Do actual update
-        destroy_without_disable_update
-
-        # Reenable updates to perform a single update
-        GitHostingObserver.set_update_active(true);
       end
 
     end
