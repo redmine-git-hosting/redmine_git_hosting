bc6f0899e68ca87e1b502c699cab32b5aabe039a
Nicolas <nrodriguez@jbox-web.com>
2013-09-14 18:41:03 +0200

    Add spaces

--- lib/redmine_git_hosting/patches/project_patch.rb
+++ lib/redmine_git_hosting/patches/project_patch.rb
@@ -23,6 +23,7 @@ module RedmineGitHosting
         end
       end
 
+
       module InstanceMethods
 
         # Find all repositories owned by project which are Repository::Git
@@ -43,7 +44,7 @@ module RedmineGitHosting
 
         # Return first repo with a blank identifier (should be only one!)
         def repo_blank_ident
-          Repository.find_by_project_id(id,:conditions => ["identifier = '' or identifier is null"])
+          Repository.find_by_project_id(id, :conditions => ["identifier = '' or identifier is null"])
         end
 
         private
@@ -51,8 +52,8 @@ module RedmineGitHosting
         # Make sure that identifier does not match existing repository identifier
         # Only for Redmine 1.4
         def additional_ident_constraints
-          if new_record? && !identifier.blank? && Repository.find_by_identifier_and_type(identifier,"Git")
-            errors.add(:identifier,:ident_not_unique)
+          if new_record? && !identifier.blank? && Repository.find_by_identifier_and_type(identifier, "Git")
+            errors.add(:identifier, :ident_not_unique)
           end
         end
 
