f7307bc0cd69f634537c9249b6d860ddcb0bf308
root <root@dev.jbox-web.fr>
2013-09-16 02:48:56 +0200

    JS for ZeroClipboard is framework independant

---
+++ assets/javascripts/set_zero_clipboard.js
@@ -0,0 +1,25 @@
+var zero_clipboard_source_input_control_id = "git_url_text";
+
+function setZeroClipboardInputSource(id) {
+  zero_clipboard_source_input_control_id = id;
+}
+
+var button      = null;
+var source_text = null;
+var clipboard   = null;
+
+function setUpClipboard() {
+  var clip_container = document.getElementById('clipboard_container');
+  if (clip_container) {
+    clip_container.show();
+    var button      = document.getElementById('clipboard_button');
+    var clipboard   = new ZeroClipboard(button);
+
+    clipboard.on('mouseover', function (client, args) {
+      var source_text = document.getElementById(zero_clipboard_source_input_control_id).value;
+      clipboard.setText(source_text);
+    });
+  }
+}
+
+document.observe("dom:loaded", function(){ setUpClipboard(); });
