b523cd9e50e5479b7145c3b5a0d08e61d29c2e7b
root <root@dev.jbox-web.fr>
2014-02-23 22:34:56 +0100

    Remove useless methods (Notify CIA.vc related)

--- app/helpers/git_hosting_helper.rb
+++ app/helpers/git_hosting_helper.rb
@@ -153,61 +153,6 @@ module GitHostingHelper
   end
 
 
-  def url_for_revision(revision)
-    rev = revision.respond_to?(:identifier) ? revision.identifier : revision
-    shorten_url(
-      url_for(:controller => 'repositories', :action => 'revision', :id => revision.project,
-        :rev => rev, :only_path => false, :host => Setting['host_name'], :protocol => Setting['protocol']
-      )
-    )
-  end
-
-
-  def url_for_revision_path(revision, path)
-    rev = revision.respond_to?(:identifier) ? revision.identifier : revision
-    shorten_url(
-      url_for(:controller => 'repositories', :action => 'entry', :id => revision.project,
-        :rev => rev, :path => path, :only_path => false, :host => Setting['host_name'],
-        :protocol => Setting['protocol']
-      )
-    )
-  end
-
-  @@file_actions = {
-    "a" => "add",
-    "m" => "modify",
-    "r" => "remove",
-    "d" => "remove"
-  }
-
-
-  def map_file_action(action)
-    @@file_actions.fetch(action.downcase, action)
-  end
-
-  @http_server = nil
-
-  def shorten_url(url)
-    if @http_server.nil?
-      @uri = URI.parse("http://tinyurl.com/api-create.php")
-      @http_server = Net::HTTP.new(@uri.host, @uri.port)
-      @http_server.open_timeout = 1 # in seconds
-      @http_server.read_timeout = 1 # in seconds
-    end
-    uri = @uri
-    uri.query = "url=#{url}"
-    request = Net::HTTP::Get.new(uri.request_uri)
-    begin
-      response = @http_server.request(request)
-      GitHosting.logger.debug "Shortened URL is: #{response.body}"
-      return response.body
-    rescue Exception => e
-      GitHosting.logger.warn "Failed to shorten url: #{e}"
-      return url
-    end
-  end
-
-
   # Generic helper functions
   def reldir_add_dotslash(path)
     # Is this a relative path?
