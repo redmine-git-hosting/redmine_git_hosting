862d27f47bbdb4b469c63bde5b32240f8972975f
root <root@dev.jbox-web.fr>
2014-03-05 07:18:39 +0100

    Drop Notify CIA table

---
+++ db/migrate/20140305053200_remove_notify_cia.rb
@@ -0,0 +1,17 @@
+class RemoveNotifyCia < ActiveRecord::Migration
+
+  def self.up
+    drop_table :git_cia_notifications
+
+    remove_column :repository_git_extras, :notify_cia
+  end
+
+
+  def self.down
+    create_table :git_cia_notifications do |t|
+      t.column :repository_id, :integer
+      t.column :scmid, :string
+    end
+  end
+
+end
