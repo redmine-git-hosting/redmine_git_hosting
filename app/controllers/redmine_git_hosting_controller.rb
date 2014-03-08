fb630b4ffd63e48ac985ece078836f7d39e5c06a
root <root@dev.jbox-web.fr>
2014-03-08 05:16:21 +0100

    Dry controllers :)

---
+++ app/controllers/redmine_git_hosting_controller.rb
@@ -0,0 +1,67 @@
+class RedmineGitHostingController < ApplicationController
+  unloadable
+
+  before_filter :require_login
+  before_filter :set_repository_variable
+  before_filter :set_project_variable
+  before_filter :check_required_permissions
+  before_filter :check_xhr_request
+
+  layout Proc.new { |controller| controller.request.xhr? ? 'popup' : 'base' }
+
+  include GitHostingHelper
+  helper  :git_hosting
+
+
+  protected
+
+
+  def set_repository_variable
+    @repository = Repository.find_by_id(params[:repository_id])
+    if @repository.nil?
+      render_404
+    end
+  end
+
+
+  def set_project_variable
+    @project = @repository.project
+    if @project.nil?
+      render_404
+    end
+  end
+
+
+  def check_required_permissions
+    # Deny access if the current user is not allowed to manage the project's repository
+    if !@project.module_enabled?(:repository)
+      render_403
+    end
+
+    return true if User.current.admin?
+
+    not_enough_perms = true
+
+    User.current.roles_for_project(@project).each do |role|
+      if role.allowed_to?(:manage_repository)
+        not_enough_perms = false
+        break
+      end
+    end
+
+    if not_enough_perms
+      render_403
+    end
+  end
+
+
+  def success_url
+    url_for(:controller => 'repositories', :action => 'edit', :id => @repository.id)
+  end
+
+
+  def check_xhr_request
+    @is_xhr ||= request.xhr?
+  end
+
+end
--- app/controllers/repository_git_config_keys_controller.rb
+++ app/controllers/repository_git_config_keys_controller.rb
@@ -1,17 +1,7 @@
-class RepositoryGitConfigKeysController < ApplicationController
+class RepositoryGitConfigKeysController < RedmineGitHostingController
   unloadable
 
-  before_filter :require_login
-  before_filter :set_repository_variable
-  before_filter :set_project_variable
-  before_filter :check_required_permissions
-  before_filter :check_xhr_request
   before_filter :find_repository_git_config_key, :except => [:index, :new, :create]
-
-  layout Proc.new { |controller| controller.request.xhr? ? 'popup' : 'base' }
-
-  include GitHostingHelper
-  helper  :git_hosting
 
 
   def index
@@ -92,28 +82,6 @@ class RepositoryGitConfigKeysController < ApplicationController
   protected
 
 
-  # This is a success URL to return to basic listing
-  def success_url
-    url_for(:controller => 'repositories', :action => 'edit', :id => @repository.id)
-  end
-
-
-  def set_repository_variable
-    @repository = Repository.find_by_id(params[:repository_id])
-    if @repository.nil?
-      render_404
-    end
-  end
-
-
-  def set_project_variable
-    @project = @repository.project
-    if @project.nil?
-      render_404
-    end
-  end
-
-
   def find_repository_git_config_key
     git_config_key = RepositoryGitConfigKey.find_by_id(params[:id])
 
@@ -126,32 +94,5 @@ class RepositoryGitConfigKeysController < ApplicationController
     end
   end
 
-
-  def check_required_permissions
-    # Deny access if the current user is not allowed to manage the project's repository
-    if !@project.module_enabled?(:repository)
-      render_403
-    end
-
-    return true if User.current.admin?
-
-    not_enough_perms = true
-
-    User.current.roles_for_project(@project).each do |role|
-      if role.allowed_to?(:manage_repository)
-        not_enough_perms = false
-        break
-      end
-    end
-
-    if not_enough_perms
-      render_403
-    end
-  end
-
-
-  def check_xhr_request
-    @is_xhr ||= request.xhr?
-  end
 
 end
