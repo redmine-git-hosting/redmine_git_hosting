e4cfbd10b936a10207b0b981152b429e5aaac122
root <root@dev.jbox-web.fr>
2014-03-09 18:18:52 +0100

    Remove useless methods

--- app/helpers/git_hosting_helper.rb
+++ app/helpers/git_hosting_helper.rb
@@ -27,27 +27,6 @@ module GitHostingHelper
   end
 
 
-  ## GIT DAEMON ENABLED?
-  def git_daemon_enabled(repository, value)
-    gd = 1
-    if repository && !repository.extra.nil?
-      gd = repository.extra[:git_daemon] ? repository.extra[:git_daemon] : gd
-    end
-    gd = repository.project.is_public ? gd : 0
-    return return_selected_string(gd, value)
-  end
-
-
-  ## SMART HTTP ENABLED?
-  def git_http_enabled(repository, value)
-    gh = 1
-    if repository && !repository.extra.nil?
-      gh = repository.extra[:git_http] ? repository.extra[:git_http] : gh
-    end
-    return return_selected_string(gh, value)
-  end
-
-
   # Post-receive Mode
   def post_receive_mode(prurl)
     if prurl.active == 0
@@ -56,21 +35,6 @@ module GitHostingHelper
       l(:label_github_post)
     else
       l(:label_empty_get)
-    end
-  end
-
-
-  # Refspec for mirrors
-  def refspec(mirror, max_refspec=0)
-    if mirror.push_mode == RepositoryMirror::PUSHMODE_MIRROR
-      l(:all_references)
-    else
-      result = []
-      result << l(:all_branches) if mirror.include_all_branches
-      result << l(:all_tags) if mirror.include_all_tags
-      result << mirror.explicit_refspec if (max_refspec == 0) || ((1..max_refspec) === mirror.explicit_refspec.length)
-      result << l(:explicit) if (max_refspec > 0) && (mirror.explicit_refspec.length > max_refspec)
-      result.join(",<br />")
     end
   end
 
@@ -85,9 +49,18 @@ module GitHostingHelper
   end
 
 
-  def return_selected_string(found_value, to_check_value)
-    return "selected='selected'" if (found_value == to_check_value)
-    return ""
+  # Refspec for mirrors
+  def refspec(mirror, max_refspec = 0)
+    if mirror.push_mode == RepositoryMirror::PUSHMODE_MIRROR
+      l(:all_references)
+    else
+      result = []
+      result << l(:all_branches) if mirror.include_all_branches
+      result << l(:all_tags) if mirror.include_all_tags
+      result << mirror.explicit_refspec if (max_refspec == 0) || ((1..max_refspec) === mirror.explicit_refspec.length)
+      result << l(:explicit) if (max_refspec > 0) && (mirror.explicit_refspec.length > max_refspec)
+      result.join(",<br />")
+    end
   end
 
 
