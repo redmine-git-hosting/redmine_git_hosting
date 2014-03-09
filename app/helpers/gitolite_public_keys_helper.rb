7bd89e850429bc1644b87a5e91ee3c46755af457
root <root@dev.jbox-web.fr>
2014-03-10 00:26:16 +0100

    Remove non used method

--- app/helpers/gitolite_public_keys_helper.rb
+++ app/helpers/gitolite_public_keys_helper.rb
@@ -1,13 +1,5 @@
 module GitolitePublicKeysHelper
 
-  def gitolite_public_keys_status_options_for_select(user, selected)
-    key_count_by_active = user.gitolite_public_keys.count(:group => 'active').to_hash
-    options_for_select([[l(:label_all), nil],
-          ["#{l(:status_active)} (#{key_count_by_active[true].to_i})", GitolitePublicKey::STATUS_ACTIVE],
-          ["#{l(:status_locked)} (#{key_count_by_active[false].to_i})", GitolitePublicKey::STATUS_LOCKED]], selected)
-  end
-
-
   def keylabel(key)
     if key.user == User.current
       "\"#{key.title}\""
