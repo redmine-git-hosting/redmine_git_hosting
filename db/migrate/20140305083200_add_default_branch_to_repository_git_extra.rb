1d6b111309613c20c9fc6b3d3bd7cb8f0bfbdd1a
root <root@dev.jbox-web.fr>
2014-04-16 14:15:53 +0200

    Fix https://github.com/jbox-web/redmine_git_hosting/issues/160

--- db/migrate/20140305083200_add_default_branch_to_repository_git_extra.rb
+++ db/migrate/20140305083200_add_default_branch_to_repository_git_extra.rb
@@ -1,7 +1,14 @@
 class AddDefaultBranchToRepositoryGitExtra < ActiveRecord::Migration
 
   def self.up
-    add_column :repository_git_extras, :default_branch, :string, :null => false, :after => :git_notify
+    add_column :repository_git_extras, :default_branch, :string, :after => :git_notify
+
+    RepositoryGitExtra.reset_column_information
+    RepositoryGitExtra.all.each do |extra|
+      extra.update_attribute(:default_branch, 'master')
+    end
+
+    change_column :repository_git_extras, :default_branch, :string, :null => false
   end
 
   def self.down
