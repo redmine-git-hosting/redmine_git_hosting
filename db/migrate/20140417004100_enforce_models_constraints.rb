50c1ddeb9576daf079ce302265c1c69eea45cd0e
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2014-04-29 13:33:58 +0200

    fix db migration on postgressql server

--- db/migrate/20140417004100_enforce_models_constraints.rb
+++ db/migrate/20140417004100_enforce_models_constraints.rb
@@ -10,14 +10,14 @@ class EnforceModelsConstraints < ActiveRecord::Migration
     change_column :gitolite_public_keys, :key_type,   :integer, :null => false, :after => :user_id
     change_column :gitolite_public_keys, :title,      :string, :null => false
     change_column :gitolite_public_keys, :identifier, :string, :null => false
-    change_column :gitolite_public_keys, :key,        :string, :null => false
-    change_column :gitolite_public_keys, :active,     :boolean, :default => true
+    change_column :gitolite_public_keys, :key,        :text, :null => false
+    change_column :gitolite_public_keys, :active,     :integer, :default => 1
     change_column :gitolite_public_keys, :delete_when_unused, :boolean, :default => true, :after => :active
 
     change_column :repository_deployment_credentials, :repository_id, :integer, :null => false
     change_column :repository_deployment_credentials, :gitolite_public_key_id, :integer, :null => false
     change_column :repository_deployment_credentials, :user_id, :integer, :null => false
-    change_column :repository_deployment_credentials, :active, :boolean, :default => true
+    change_column :repository_deployment_credentials, :active, :integer, :default => 1
 
     change_column :repository_git_config_keys, :repository_id, :integer, :null => false
     change_column :repository_git_config_keys, :key,           :string,  :null => false
@@ -32,7 +32,7 @@ class EnforceModelsConstraints < ActiveRecord::Migration
 
     change_column :repository_mirrors, :repository_id, :integer, :null => false, :after => :id
     change_column :repository_mirrors, :url,           :string,  :null => false, :after => :repository_id
-    change_column :repository_mirrors, :active,        :boolean, :default => true
+    change_column :repository_mirrors, :active,        :integer, :default => 1
     change_column :repository_mirrors, :push_mode,     :string,  :null => false
     remove_column :repository_mirrors, :created_at
     remove_column :repository_mirrors, :updated_at
@@ -40,7 +40,7 @@ class EnforceModelsConstraints < ActiveRecord::Migration
     change_column :repository_post_receive_urls, :repository_id, :integer, :null => false, :after => :id
     change_column :repository_post_receive_urls, :url,           :string,  :null => false, :after => :repository_id
     change_column :repository_post_receive_urls, :mode,          :string,  :null => false, :after => :url
-    change_column :repository_post_receive_urls, :active,        :boolean, :default => true
+    change_column :repository_post_receive_urls, :active,        :integer, :default => 1
     remove_column :repository_post_receive_urls, :created_at
     remove_column :repository_post_receive_urls, :updated_at
 
