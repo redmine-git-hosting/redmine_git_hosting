97f3896139bec5a9bff683e01885f8dafb633e83
root <root@dev.jbox-web.fr>
2014-05-15 15:13:54 +0200

    Reload config before testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec/spec_helper')
+
+## Configure RSpec
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    RedmineGitolite::ConfigRedmine.reload_config
+  end
+
+end
