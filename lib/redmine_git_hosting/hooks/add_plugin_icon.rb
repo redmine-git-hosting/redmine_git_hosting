0bd035bee2a6d718d12b7ca855787d3c940361e2
root <root@dev.jbox-web.fr>
2014-05-31 11:23:38 +0200

    Add \n to html headers

--- lib/redmine_git_hosting/hooks/add_plugin_icon.rb
+++ lib/redmine_git_hosting/hooks/add_plugin_icon.rb
@@ -2,8 +2,10 @@ module RedmineGitHosting
   module Hooks
     class AddPluginIcon < Redmine::Hook::ViewListener
 
-      def view_layouts_base_html_head(context={})
-        return stylesheet_link_tag(:application, :plugin => 'redmine_git_hosting')
+      def view_layouts_base_html_head(context = {})
+        header = ""
+        header << stylesheet_link_tag(:application, :plugin => 'redmine_git_hosting') + "\n"
+        header
       end
 
     end
