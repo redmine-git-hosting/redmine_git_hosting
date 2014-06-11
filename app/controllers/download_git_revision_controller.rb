1b153c3c0866c33087f4a76ec380e8fa77a7cda9
root <root@dev.jbox-web.fr>
2014-06-11 04:34:10 +0200

    Puts DownloadGitRevision logic in Service object

--- app/controllers/download_git_revision_controller.rb
+++ app/controllers/download_git_revision_controller.rb
@@ -10,81 +10,31 @@ class DownloadGitRevisionController < ApplicationController
 
 
   def index
-    commit = nil
+    if !params[:rev]
+      revision = "master"
+    else
+      revision = params[:rev]
+    end
+
     format = params[:download_format]
 
-    if !params[:rev]
-      rev = "master"
-    else
-      rev = params[:rev]
-    end
+    download = DownloadGitRevision.new(@repository, revision, format)
 
-    # is the revision a branch?
-    @repository.branches.each do |x|
-      if x.to_s == rev
-        commit = x.revision
-        break
-      end
-    end
-
-    # is the revision a tag?
-    if commit.nil?
-      tags = RedmineGitolite::GitHosting.execute_command(:git_cmd, "--git-dir='#{@repository.gitolite_repository_path}' tag").split
-      tags.each do |x|
-        if x == rev
-          commit = RedmineGitolite::GitHosting.execute_command(:git_cmd, "--git-dir='#{@repository.gitolite_repository_path}' rev-list #{rev}").split[0]
-          break
-        end
-      end
-    end
-
-    # well, let check if this is a commit then
-    if commit.nil?
-      commit = rev
-    end
-
-    valid_commit = RedmineGitolite::GitHosting.execute_command(:git_cmd, "--git-dir='#{@repository.gitolite_repository_path}' rev-parse --quiet --verify #{commit}")
-
-    if valid_commit == ''
-      flash.now[:error] = l(:error_download_revision_no_such_commit, :commit => commit)
+    if !download.commit_valid
+      flash.now[:error] = l(:error_download_revision_no_such_commit, :commit => revision)
       render_404
       return
     end
 
-    project_name = @project.to_s.parameterize.to_s
-
-    if project_name.length == 0
-      project_name = "tarball"
-    end
-
-    cmd_args = ""
-
-    case format
-      when 'tar' then
-        extension = 'tar'
-        content_type = 'application/x-tar'
-        cmd_args << " --format=tar"
-      when 'tar.gz' then
-        extension = 'tar.gz'
-        content_type = 'application/x-gzip'
-        cmd_args << " --format=tar.gz"
-        cmd_args << " -7"
-      when 'zip' then
-        extension = 'zip'
-        content_type = 'application/x-zip'
-        cmd_args << " --format=zip"
-        cmd_args << " -7"
-    end
-
     begin
-      content = RedmineGitolite::GitHosting.execute_command(:git_cmd, "--git-dir='#{@repository.gitolite_repository_path}' archive #{cmd_args} #{valid_commit}")
+      content = download.content
     rescue => e
       flash.now[:error] = l(:git_archive_timeout, :timeout => e.message)
       render_404
       return
     end
 
-    send_data(content, :filename => "#{project_name}-#{rev}.#{extension}", :type => content_type)
+    send_data(content, :filename => download.filename, :type => download.content_type)
   end
 
 
