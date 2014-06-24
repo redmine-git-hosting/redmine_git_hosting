898692796f49022ce7ea3049106202299138ff6b
root <root@dev.jbox-web.fr>
2014-06-24 02:33:23 +0200

    We'll create unique index later, column is empty for now

--- db/migrate/20140618224954_add_fingerprint_to_gitolite_public_keys.rb
+++ db/migrate/20140618224954_add_fingerprint_to_gitolite_public_keys.rb
@@ -2,12 +2,10 @@ class AddFingerprintToGitolitePublicKeys < ActiveRecord::Migration
 
   def self.up
     add_column :gitolite_public_keys, :fingerprint, :string, :null => false, :after => 'key'
-    add_index  :gitolite_public_keys, :fingerprint, :unique => true
   end
 
 
   def self.down
-    remove_index  :gitolite_public_keys, :fingerprint
     remove_column :gitolite_public_keys, :fingerprint
   end
 
