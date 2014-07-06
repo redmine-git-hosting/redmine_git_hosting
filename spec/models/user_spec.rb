eff61ebdea4b012d03fdeb66a9c65363287a7e46
root <root@dev.jbox-web.fr>
2014-07-07 01:53:28 +0200

    Add tests for patched User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe User do
+
+  before(:all) do
+    @user = FactoryGirl.create(:user)
+  end
+
+  subject { @user }
+
+  it { should be_valid }
+
+  it { should respond_to(:gitolite_identifier) }
+
+  it "has a gitolite_identifier" do
+    expect(@user.gitolite_identifier).to match(/redmine_user\d+_\d+/)
+  end
+end
