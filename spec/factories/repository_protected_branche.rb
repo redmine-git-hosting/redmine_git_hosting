c7148c35cd32b5ca9bcf01fabb3778961f635e85
root <root@dev.jbox-web.fr>
2014-07-10 23:42:04 +0200

    Add tests for RepositoryProtectedBranche

---
+++ spec/factories/repository_protected_branche.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+
+  factory :repository_protected_branche do |protected_branch|
+
+  end
+
+end
---
+++ spec/models/repository_protected_branche_spec.rb
@@ -0,0 +1,49 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe RepositoryProtectedBranche do
+
+  before(:all) do
+    @project    = FactoryGirl.create(:project)
+    @repository = FactoryGirl.create(:repository, :project_id => @project.id)
+  end
+
+
+  def build_protected_branch(opts = {})
+    opts = opts.merge(:repository_id => @repository.id)
+    FactoryGirl.build(:repository_protected_branche, opts)
+  end
+
+
+  describe "Valid RepositoryProtectedBranche creation" do
+    before do
+      @protected_branch = build_protected_branch(:path => 'devel', :permissions => 'RW', :user_list => %w(user1 user2))
+    end
+
+    subject { @protected_branch }
+
+    ## Attributes
+    it { should allow_mass_assignment_of(:path) }
+    it { should allow_mass_assignment_of(:permissions) }
+    it { should allow_mass_assignment_of(:position) }
+    it { should allow_mass_assignment_of(:user_list) }
+
+    ## Relations
+    it { should belong_to(:repository) }
+
+    ## Validations
+    it { should be_valid }
+
+    it { should validate_presence_of(:repository_id) }
+    it { should validate_presence_of(:path) }
+    it { should validate_presence_of(:permissions) }
+    it { should validate_presence_of(:user_list) }
+
+    it {
+      should ensure_inclusion_of(:permissions).
+      in_array(%w(RW RW+))
+    }
+
+    ## Serializations
+    it { should serialize(:user_list) }
+  end
+end
