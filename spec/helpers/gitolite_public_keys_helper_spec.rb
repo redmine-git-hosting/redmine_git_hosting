8e8c9e3984e07e249cb5035df81261f3ac23d8e2
root <root@dev.jbox-web.fr>
2014-07-14 01:16:05 +0200

    Add test for can_create_deployment_keys_for_some_project method

--- spec/helpers/gitolite_public_keys_helper_spec.rb
+++ spec/helpers/gitolite_public_keys_helper_spec.rb
@@ -5,23 +5,51 @@ describe GitolitePublicKeysHelper do
   TEST_KEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpOU1DzQzU4/acdt3wWhk43acGs3Jp7jVlnEtc+2C8QFAUiJMrAOzyUnEliwxarGonJ5gKbI9NkqqPpz9LATBQw382+3FjAlptgqn7eGBih0DgwN6wdHflTRdE6sRn7hxB5h50p547n26FpbX9GSOHPhgxSnyvGXnC+YZyTfMiw5JMhw68SfLS8YENrXukg2ItJPspn6mPqIHrcM2NJOG4Bm+1ibYpDfrWJqYp3Q6disgwrsN08pS6lDfoQRiRHXg8WFbQbHloVaYFpdT6VoBQiAydeSpDSYTBJd/v3qTpK8aheC8sdnrddZf1T6L51z7WZ6vPVKQYPjpAxZ4p6eef nicolas@tchoum'
 
   before(:all) do
-    @admin_user = FactoryGirl.create(:user, :admin => true)
-    @user       = FactoryGirl.create(:user, :admin => false)
+    @admin_user         = FactoryGirl.create(:user, :admin => true)
 
-    @gitolite_public_key = FactoryGirl.create(:gitolite_public_key, :user_id => @user.id, :key_type => 1, :title => 'foo1', :key => TEST_KEY)
+    @user_without_perm  = FactoryGirl.create(:user)
+    @user_with_perm     = create_user_with_permissions(FactoryGirl.create(:project))
+
+    @gitolite_public_key = FactoryGirl.create(:gitolite_public_key, :user_id => @user_without_perm.id, :key_type => 1, :title => 'foo1', :key => TEST_KEY)
+  end
+
+
+  def create_user_with_permissions(project)
+    role = FactoryGirl.create(:role)
+    role.permissions << :create_deployment_keys
+    role.save!
+
+    user = FactoryGirl.create(:user)
+
+    members = Member.new(:role_ids => [role.id], :user_id => user.id)
+    project.members << members
+
+    return user
   end
 
 
   describe ".keylabel" do
     context "when current user is the key owner" do
-      before { User.current = @user }
+      before { User.current = @user_without_perm }
       it { expect(helper.keylabel(@gitolite_public_key)).to eq 'foo1' }
     end
 
     context "when current user is not the key owner" do
       before { User.current = @admin_user }
-      it { expect(helper.keylabel(@gitolite_public_key)).to eq 'user2@foo1' }
+      it { expect(helper.keylabel(@gitolite_public_key)).to eq 'user11@foo1' }
     end
   end
 
+  describe ".can_create_deployment_keys_for_some_project"
+    context "when current user is admin" do
+      it { expect(helper.can_create_deployment_keys_for_some_project(@admin_user)).to eq true }
+    end
+
+    context "when current user can create_deployment_keys" do
+      it { expect(helper.can_create_deployment_keys_for_some_project(@user_with_perm)).to eq true }
+    end
+
+    context "when current user cannot create_deployment_keys" do
+      it { expect(helper.can_create_deployment_keys_for_some_project(@user_without_perm)).to eq false }
+    end
 end
