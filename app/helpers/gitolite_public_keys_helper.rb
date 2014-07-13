dc7670a72905ad94723f979407d39ca28e7a75ec
root <root@dev.jbox-web.fr>
2014-07-14 01:01:35 +0200

    Add tests for GitolitePublicKeysHelper

--- app/helpers/gitolite_public_keys_helper.rb
+++ app/helpers/gitolite_public_keys_helper.rb
@@ -2,15 +2,6 @@ module GitolitePublicKeysHelper
 
   def keylabel(key)
     if key.user == User.current
-      "'#{key.title}'"
-    else
-      "'#{key.user.login}@#{key.title}'"
-    end
-  end
-
-
-  def keylabel_text(key)
-    if key.user == User.current
       "#{key.title}"
     else
       "#{key.user.login}@#{key.title}"
