78dabb2e33794359f4027a03975bbeaa56e69e22
root <root@dev.jbox-web.fr>
2014-07-14 16:01:59 +0200

    Fix repo order with Postgres

--- lib/redmine_git_hosting/patches/project_patch.rb
+++ lib/redmine_git_hosting/patches/project_patch.rb
@@ -22,7 +22,7 @@ module RedmineGitHosting
 
         # Find all repositories owned by project which are Repository::Git
         def gitolite_repos
-          repositories.select{|x| x.is_a?(Repository::Git)}.sort{|x, y| x.id <=> x.id}
+          repositories.select{|x| x.is_a?(Repository::Git)}.sort{|x, y| x.id <=> y.id}
         end
 
         # Return first repo with a blank identifier (should be only one!)
