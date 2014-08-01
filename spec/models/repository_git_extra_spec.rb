ef40f7e6c1c6a1b9d1e4c36ab1ad96c65fa72ff3
root <root@dev.jbox-web.fr>
2014-08-01 02:17:25 +0200

    Try to fix uniqueness failure

--- spec/models/repository_git_extra_spec.rb
+++ spec/models/repository_git_extra_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe RepositoryGitExtra do
 
   describe "Valid RepositoryGitExtra creation" do
-    before do
+    before(:each) do
       @git_extra = build(:repository_git_extra)
     end
 
