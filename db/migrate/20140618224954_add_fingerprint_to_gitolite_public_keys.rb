e8fe48a5a455a7cd1f001dfde82ae9953ce2f95e
Strnadj <jan.strnadek@gmail.com>
2014-09-16 13:48:19 +0200

    Fix migration

--- db/migrate/20140618224954_add_fingerprint_to_gitolite_public_keys.rb
+++ db/migrate/20140618224954_add_fingerprint_to_gitolite_public_keys.rb
@@ -1,7 +1,13 @@
 class AddFingerprintToGitolitePublicKeys < ActiveRecord::Migration
 
   def self.up
-    add_column :gitolite_public_keys, :fingerprint, :string, :null => false, :after => 'key'
+    add_column :gitolite_public_keys, :fingerprint, :string, :after => 'key'
+    say_with_time 'Reset identifiers in exists repositories' do
+       GitolitePublicKey.all.each do |key|
+         key.reset_identifiers
+       end
+    end
+    change_column :gitolite_public_keys, :fingeprint, :string, :null => false
   end
 
 
