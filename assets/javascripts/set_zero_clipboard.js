692f404df161a1d3bf0b40bf71e89397456c3774
root <root@dev.jbox-web.fr>
2014-09-21 06:26:50 +0200

    Fix ZeroClipboard init

--- assets/javascripts/set_zero_clipboard.js
+++ assets/javascripts/set_zero_clipboard.js
@@ -2,16 +2,18 @@ function createZeroClipBoard(object){
   ZeroClipboard.config({ swfPath: object.movie_path });
   var client = new ZeroClipboard($(object.target));
 
-  $('#global-zeroclipboard-html-bridge').tooltip({
-    title:     object.label_to_copy,
-    placement: 'right'
-  });
+  client.on('ready', function() {
+    $('#global-zeroclipboard-html-bridge').tooltip({
+      title:     object.label_to_copy,
+      placement: 'right'
+    });
 
-  client.on('mouseover', function() {
-    $('#global-zeroclipboard-html-bridge').tooltip('show');
-  });
+    client.on('beforecopy', function() {
+      $('#global-zeroclipboard-html-bridge').tooltip('show');
+    });
 
-  client.on('complete', function() {
-    $('.tooltip .tooltip-inner').text(object.label_copied);
+    client.on('aftercopy', function() {
+      $('.tooltip .tooltip-inner').text(object.label_copied);
+    });
   });
 }
