c01f0973448dc8fa77b01af62d874f706d62d2bb
root <root@dev.jbox-web.fr>
2014-09-21 02:46:41 +0200

    Fix rare nil cases with refactoring

--- lib/redmine_gitolite/gitolite_modules/gitolite_infos.rb
+++ lib/redmine_gitolite/gitolite_modules/gitolite_infos.rb
@@ -17,6 +17,36 @@ module RedmineGitolite::GitoliteModules
 
     module ClassMethods
 
+      @@gitolite_infos_cached = nil
+      @@gitolite_info_stamp   = nil
+
+      def gitolite_infos
+        if !@@gitolite_infos_cached.nil? && (Time.new - @@gitolite_info_stamp <= 1)
+          return @@gitolite_infos_cached
+        end
+        begin
+          @@gitolite_infos_cached = ssh_shell('info')[0]
+        rescue RedmineGitolite::GitHosting::GitHostingException => e
+          logger.error { "Error while getting Gitolite version" }
+          @@gitolite_infos_cached = ''
+        end
+        @@gitolite_info_stamp = Time.new
+        return @@gitolite_infos_cached
+      end
+
+
+      def gitolite_version
+        logger.debug { "Getting Gitolite version..." }
+        find_version(gitolite_infos)
+      end
+
+
+      def gitolite_banner
+        logger.debug { "Getting Gitolite banner..." }
+        gitolite_infos
+      end
+
+
       def find_version(output)
         return 0 if output.blank?
 
@@ -48,73 +78,20 @@ module RedmineGitolite::GitoliteModules
       end
 
 
-      @@gitolite_version_cached = nil
-      @@gitolite_version_stamp  = nil
-
-      def gitolite_version
-        if !@@gitolite_version_cached.nil? && (Time.new - @@gitolite_version_stamp <= 1)
-          return @@gitolite_version_cached
-        end
-
-        logger.debug { "Getting Gitolite version..." }
-
-        begin
-          out, err, code = ssh_shell('info')
-          @@gitolite_version_cached = find_version(out)
-        rescue RedmineGitolite::GitHosting::GitHostingException => e
-          logger.error { "Error while getting Gitolite version" }
-          @@gitolite_version_cached = -1
-        end
-
-        @@gitolite_version_stamp = Time.new
-        return @@gitolite_version_cached
-      end
-
-
-      @@gitolite_banner_cached = nil
-      @@gitolite_banner_stamp  = nil
-
-      def gitolite_banner
-        if !@@gitolite_banner_cached.nil? && (Time.new - @@gitolite_banner_stamp <= 1)
-          return @@gitolite_banner_cached
-        end
-
-        logger.debug { "Getting Gitolite banner..." }
-
-        begin
-          @@gitolite_banner_cached = ssh_shell('info')[0]
-        rescue RedmineGitolite::GitHosting::GitHostingException => e
-          logger.error { "Error while getting Gitolite banner" }
-          @@gitolite_banner_cached = "Error : #{e.message}"
-        end
-
-        @@gitolite_banner_stamp = Time.new
-        return @@gitolite_banner_cached
-      end
-
-
-      @@gitolite_repository_count_cached = nil
-      @@gitolite_repository_count_stamp  = nil
-
       def gitolite_repository_count
         if gitolite_version == 3
-          if !@@gitolite_repository_count_cached.nil? && (Time.new - @@gitolite_repository_count_stamp <= 1)
-            return @@gitolite_repository_count_cached
-          end
-
           logger.debug { "Getting Gitolite physical repositories list..." }
 
           begin
-            @@gitolite_repository_count_cached = sudo_capture('gitolite', 'list-phy-repos').split("\n").length
+            count = sudo_capture('gitolite', 'list-phy-repos').split("\n").length
           rescue RedmineGitolite::GitHosting::GitHostingException => e
             logger.error { "Error while getting Gitolite physical repositories list" }
-            @@gitolite_repository_count_cached = "Error : #{e.message}"
+            count = 0
           end
 
-          @@gitolite_repository_count_stamp = Time.new
-          return @@gitolite_repository_count_cached
+          return count
         else
-          return @@gitolite_repository_count_cached = 'This is Gitolite v2, not implemented...'
+          return 'This is Gitolite v2, not implemented...'
         end
       end
 
