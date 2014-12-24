2a127c662d14804d5db3f9cf4132558d65304f53
root <root@dev.jbox-web.fr>
2014-12-24 03:51:25 +0100

    Indent

--- app/models/gitolite_public_key.rb
+++ app/models/gitolite_public_key.rb
@@ -133,136 +133,136 @@ class GitolitePublicKey < ActiveRecord::Base
   protected
 
 
-  def add_ssh_key
-    RedmineGitolite::GitHosting.logger.info { "User '#{User.current.login}' has added a SSH key" }
-    RedmineGitolite::GitHosting.resync_gitolite(:add_ssh_key, self.id)
-  end
+    def add_ssh_key
+      RedmineGitolite::GitHosting.logger.info { "User '#{User.current.login}' has added a SSH key" }
+      RedmineGitolite::GitHosting.resync_gitolite(:add_ssh_key, self.id)
+    end
 
 
-  def destroy_ssh_key
-    RedmineGitolite::GitHosting.logger.info { "User '#{User.current.login}' has deleted a SSH key" }
-    RedmineGitolite::GitHosting.resync_gitolite(:delete_ssh_key, self.to_yaml)
-  end
+    def destroy_ssh_key
+      RedmineGitolite::GitHosting.logger.info { "User '#{User.current.login}' has deleted a SSH key" }
+      RedmineGitolite::GitHosting.resync_gitolite(:delete_ssh_key, self.to_yaml)
+    end
 
 
   private
 
 
-  # Strip leading and trailing whitespace
-  def strip_whitespace
-    self.title = title.strip rescue ''
+    # Strip leading and trailing whitespace
+    def strip_whitespace
+      self.title = title.strip rescue ''
 
-    # Don't mess with existing keys (since cannot change key text anyway)
-    if new_record?
-      self.key = key.strip rescue ''
-    end
-  end
-
-
-  # Remove control characters from key
-  def remove_control_characters
-    # Don't mess with existing keys (since cannot change key text anyway)
-    return if !new_record?
-
-    # First -- let the first control char or space stand (to divide key type from key)
-    # Really, this is catching a special case in which there is a \n between type and key.
-    # Most common case turns first space back into space....
-    self.key = key.sub(/[ \r\n\t]/, ' ')
-
-    # Next, if comment divided from key by control char, let that one stand as well
-    # We can only tell this if there is an "=" in the key. So, won't help 1/3 times.
-    self.key = key.sub(/=[ \r\n\t]/, '= ')
-
-    # Delete any remaining control characters....
-    self.key = key.gsub(/[\a\r\n\t]/, '').strip
-  end
-
-
-  # Returns the unique identifier for this key based on the key_type
-  #
-  # For user public keys, this simply is the user's gitolite_identifier.
-  # For deployment keys, we use an incrementing number.
-  def set_identifier
-    if !self.user_id.nil?
-      key_count = GitolitePublicKey.by_user(self.user).deploy_key.length + 1
-
-      case key_type
-        when KEY_TYPE_USER
-          tag = self.title.gsub(/[^0-9a-zA-Z]/, '_')
-          self.identifier ||= [ self.user.gitolite_identifier, '@', 'redmine_', tag ].join
-
-        when KEY_TYPE_DEPLOY
-          self.identifier ||= [ self.user.gitolite_identifier, '_', DEPLOY_PSEUDO_USER, '_', key_count, '@', 'redmine_', DEPLOY_PSEUDO_USER, '_', key_count ].join
-      end
-    else
-      nil
-    end
-  end
-
-
-  def set_fingerprint
-    file = Tempfile.new('keytest')
-    file.write(self.key)
-    file.close
-
-    begin
-      output = RedmineGitolite::GitHosting.capture('ssh-keygen', ['-l', '-f', file.path])
-      if output
-        self.fingerprint = output.split[1]
-      end
-    rescue RedmineGitolite::GitHosting::GitHostingException => e
-      errors.add(:key, l(:error_key_corrupted))
-    ensure
-      file.unlink
-    end
-  end
-
-
-  def has_not_been_changed
-    return if new_record?
-
-    valid = true
-
-    %w(identifier key user_id key_type title fingerprint).each do |attribute|
-      method = "#{attribute}_changed?"
-      if self.send(method)
-        errors.add(attribute, 'cannot be changed')
-        valid = false
+      # Don't mess with existing keys (since cannot change key text anyway)
+      if new_record?
+        self.key = key.strip rescue ''
       end
     end
 
-    return valid
-  end
+
+    # Remove control characters from key
+    def remove_control_characters
+      # Don't mess with existing keys (since cannot change key text anyway)
+      return if !new_record?
+
+      # First -- let the first control char or space stand (to divide key type from key)
+      # Really, this is catching a special case in which there is a \n between type and key.
+      # Most common case turns first space back into space....
+      self.key = key.sub(/[ \r\n\t]/, ' ')
+
+      # Next, if comment divided from key by control char, let that one stand as well
+      # We can only tell this if there is an "=" in the key. So, won't help 1/3 times.
+      self.key = key.sub(/=[ \r\n\t]/, '= ')
+
+      # Delete any remaining control characters....
+      self.key = key.gsub(/[\a\r\n\t]/, '').strip
+    end
 
 
-  def key_correctness
-    return false if self.key.nil?
-    # Test correctness of fingerprint from output
-    # and general ssh-(r|d|ecd)sa <key> <id> structure
-    (self.key.match(/^(\S+)\s+(\S+)/)) && (self.fingerprint =~ /^(\w{2}:?)+$/i)
-  end
+    # Returns the unique identifier for this key based on the key_type
+    #
+    # For user public keys, this simply is the user's gitolite_identifier.
+    # For deployment keys, we use an incrementing number.
+    def set_identifier
+      if !self.user_id.nil?
+        key_count = GitolitePublicKey.by_user(self.user).deploy_key.length + 1
 
+        case key_type
+          when KEY_TYPE_USER
+            tag = self.title.gsub(/[^0-9a-zA-Z]/, '_')
+            self.identifier ||= [ self.user.gitolite_identifier, '@', 'redmine_', tag ].join
 
-  def key_uniqueness
-    return if !new_record?
-
-    existing = GitolitePublicKey.find_by_fingerprint(self.fingerprint)
-
-    if existing
-      # Hm.... have a duplicate key!
-      if existing.user == User.current
-        errors.add(:key, l(:error_key_in_use_by_you, :name => existing.title))
-        return false
-      elsif User.current.admin?
-        errors.add(:key, l(:error_key_in_use_by_other, :login => existing.user.login, :name => existing.title))
-        return false
+          when KEY_TYPE_DEPLOY
+            self.identifier ||= [ self.user.gitolite_identifier, '_', DEPLOY_PSEUDO_USER, '_', key_count, '@', 'redmine_', DEPLOY_PSEUDO_USER, '_', key_count ].join
+        end
       else
-        errors.add(:key, l(:error_key_in_use_by_someone))
-        return false
+        nil
       end
     end
 
-    return true
-  end
+
+    def set_fingerprint
+      file = Tempfile.new('keytest')
+      file.write(self.key)
+      file.close
+
+      begin
+        output = RedmineGitolite::GitHosting.capture('ssh-keygen', ['-l', '-f', file.path])
+        if output
+          self.fingerprint = output.split[1]
+        end
+      rescue RedmineGitolite::GitHosting::GitHostingException => e
+        errors.add(:key, l(:error_key_corrupted))
+      ensure
+        file.unlink
+      end
+    end
+
+
+    def has_not_been_changed
+      return if new_record?
+
+      valid = true
+
+      %w(identifier key user_id key_type title fingerprint).each do |attribute|
+        method = "#{attribute}_changed?"
+        if self.send(method)
+          errors.add(attribute, 'cannot be changed')
+          valid = false
+        end
+      end
+
+      return valid
+    end
+
+
+    def key_correctness
+      return false if self.key.nil?
+      # Test correctness of fingerprint from output
+      # and general ssh-(r|d|ecd)sa <key> <id> structure
+      (self.key.match(/^(\S+)\s+(\S+)/)) && (self.fingerprint =~ /^(\w{2}:?)+$/i)
+    end
+
+
+    def key_uniqueness
+      return if !new_record?
+
+      existing = GitolitePublicKey.find_by_fingerprint(self.fingerprint)
+
+      if existing
+        # Hm.... have a duplicate key!
+        if existing.user == User.current
+          errors.add(:key, l(:error_key_in_use_by_you, :name => existing.title))
+          return false
+        elsif User.current.admin?
+          errors.add(:key, l(:error_key_in_use_by_other, :login => existing.user.login, :name => existing.title))
+          return false
+        else
+          errors.add(:key, l(:error_key_in_use_by_someone))
+          return false
+        end
+      end
+
+      return true
+    end
 
 end
