558e02cd2bafbdf2d0528943046b23593295d71b
root <root@dev.jbox-web.fr>
2014-12-25 14:45:34 +0100

    Useless to validate booleans

--- app/models/repository_git_extra.rb
+++ app/models/repository_git_extra.rb
@@ -13,18 +13,10 @@ class RepositoryGitExtra < ActiveRecord::Base
   belongs_to :repository
 
   ## Validations
-  validates :repository_id,    presence: true, uniqueness: true
-
-  validates :git_http,         presence: true,
-                               numericality: { only_integer: true },
-                               inclusion:    { in: [DISABLED, HTTP, HTTPS, BOTH] }
-
-  validates :default_branch,   presence: true
-  validates :key,              presence: true
-
-  validates :git_daemon,
-  validates :git_notify,
-  validates :protected_branch,
+  validates :repository_id,  presence: true, uniqueness: true
+  validates :git_http,       presence: true, numericality: { only_integer: true }, inclusion: { in: [DISABLED, HTTP, HTTPS, BOTH] }
+  validates :default_branch, presence: true
+  validates :key,            presence: true
 
   validates_associated :repository
 
