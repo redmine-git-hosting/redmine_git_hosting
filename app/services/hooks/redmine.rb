68177e593c5a248dc7253cc32d60f17f1b6250d0
root <root@dev.jbox-web.fr>
2014-12-25 19:34:25 +0100

    Indent

--- app/services/hooks/redmine.rb
+++ app/services/hooks/redmine.rb
@@ -2,6 +2,8 @@ module Hooks
   class Redmine
     unloadable
 
+    attr_reader :repository
+
 
     def initialize(repository)
       @repository = repository
@@ -16,34 +18,34 @@ module Hooks
     private
 
 
-    def logger
-      RedmineGitolite::Log.get_logger(:git_hooks)
-    end
-
-
-    def fetch_changesets
-      ## Fetch commits from the repository
-      y = ""
-
-      logger.info { "Fetching changesets for '#{@repository.redmine_name}' repository ... " }
-      y << "  - Fetching changesets for '#{@repository.redmine_name}' repository ... "
-
-      begin
-        @repository.fetch_changesets
-        logger.info { "Succeeded!" }
-        y << " [success]\n"
-      rescue ::Redmine::Scm::Adapters::CommandFailed => e
-        logger.error { "Failed!" }
-        logger.error { "Error during fetching changesets : #{e.message}" }
-        y << " [failure]\n"
-      rescue => e
-        logger.error { "Failed!" }
-        logger.error { "Error after fetching changesets : #{e.message}" }
-        y << " [failure]\n"
+      def logger
+        RedmineGitolite::Log.get_logger(:git_hooks)
       end
 
-      return y
-    end
+
+      def fetch_changesets
+        ## Fetch commits from the repository
+        y = ""
+
+        logger.info { "Fetching changesets for '#{repository.redmine_name}' repository ... " }
+        y << "  - Fetching changesets for '#{repository.redmine_name}' repository ... "
+
+        begin
+          repository.fetch_changesets
+          logger.info { "Succeeded!" }
+          y << " [success]\n"
+        rescue ::Redmine::Scm::Adapters::CommandFailed => e
+          logger.error { "Failed!" }
+          logger.error { "Error during fetching changesets : #{e.message}" }
+          y << " [failure]\n"
+        rescue => e
+          logger.error { "Failed!" }
+          logger.error { "Error after fetching changesets : #{e.message}" }
+          y << " [failure]\n"
+        end
+
+        return y
+      end
 
   end
 end
