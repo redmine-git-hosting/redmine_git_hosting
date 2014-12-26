141665ab9e8c1e09a4be72712c3c81c5fb9daa8e
root <root@dev.jbox-web.fr>
2014-12-26 21:34:07 +0100

    Extract methods in GitolitePublicKey controller

--- app/controllers/gitolite_public_keys_controller.rb
+++ app/controllers/gitolite_public_keys_controller.rb
@@ -2,8 +2,8 @@ class GitolitePublicKeysController < ApplicationController
   unloadable
 
   before_filter :require_login
-  before_filter :set_user_variable
-  before_filter :find_gitolite_public_key, :only => [:destroy]
+  before_filter :find_user
+  before_filter :find_gitolite_public_key, only: [:destroy]
 
   helper :gitolite_public_keys
 
@@ -16,57 +16,28 @@ class GitolitePublicKeysController < ApplicationController
 
 
   def create
-    @gitolite_public_key = GitolitePublicKey.new(params[:gitolite_public_key])
-    @gitolite_public_key.user = @user
-
     if params[:create_button]
+      @gitolite_public_key = @user.gitolite_public_keys.new(params[:gitolite_public_key])
       if @gitolite_public_key.save
         flash[:notice] = l(:notice_public_key_created, :title => view_context.keylabel(@gitolite_public_key))
-
-        respond_to do |format|
-          format.html { redirect_to @redirect_url }
-        end
+        redirect_to @redirect_url
       else
         flash[:error] = @gitolite_public_key.errors.full_messages.to_sentence
-
-        respond_to do |format|
-          format.html { redirect_to @redirect_url }
-        end
+        redirect_to @redirect_url
       end
     else
-      respond_to do |format|
-        format.html { redirect_to @cancel_url }
-      end
+      redirect_to @cancel_url
     end
   end
 
 
   def destroy
     if request.delete?
-      if @gitolite_public_key.destroy
-        flash[:notice] = l(:notice_public_key_deleted, :title => view_context.keylabel(@gitolite_public_key))
-      end
-      redirect_to @redirect_url
-    end
-  end
-
-
-  private
-
-
-  def set_user_variable
-    if params[:user_id]
-      @user = (params[:user_id] == 'current') ? User.current : User.find_by_id(params[:user_id])
-      if @user
-        @cancel_url = @redirect_url = url_for(:controller => 'users', :action => 'edit', :id => params[:user_id], :tab => 'keys')
-      else
-        render_404
-      end
-    else
-      if User.current.allowed_to?(:create_gitolite_ssh_key, nil, :global => true)
-        @user = User.current
-        @redirect_url = url_for(:controller => 'gitolite_public_keys', :action => 'index')
-        @cancel_url = url_for(:controller => 'my', :action => 'account')
+      if @gitolite_public_key.user == @user || @user.admin?
+        if @gitolite_public_key.destroy
+          flash[:notice] = l(:notice_public_key_deleted, :title => view_context.keylabel(@gitolite_public_key))
+        end
+        redirect_to @redirect_url
       else
         render_403
       end
@@ -74,15 +45,43 @@ class GitolitePublicKeysController < ApplicationController
   end
 
 
-  def find_gitolite_public_key
-    key = GitolitePublicKey.find_by_id(params[:id])
-    if key && (@user == key.user || @user.admin?)
-      @gitolite_public_key = key
-    elsif key
-      render_403
-    else
+  private
+
+
+    def find_user
+      if params[:user_id]
+        set_user_from_params
+      else
+        set_user_from_current_user
+      end
+    end
+
+
+    def set_user_from_params
+      @user = (params[:user_id] == 'current') ? User.current : User.find_by_id(params[:user_id])
+      if @user
+        @cancel_url = @redirect_url = url_for(controller: 'users', action: 'edit', id: params[:user_id], tab: 'keys')
+      else
+        render_404
+      end
+    end
+
+
+    def set_user_from_current_user
+      if User.current.allowed_to?(:create_gitolite_ssh_key, nil, global: true)
+        @user = User.current
+        @redirect_url = url_for(controller: 'gitolite_public_keys', action: 'index')
+        @cancel_url = url_for(controller: 'my', action: 'account')
+      else
+        render_403
+      end
+    end
+
+
+    def find_gitolite_public_key
+      @gitolite_public_key = @user.gitolite_public_keys.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
       render_404
     end
-  end
 
 end
