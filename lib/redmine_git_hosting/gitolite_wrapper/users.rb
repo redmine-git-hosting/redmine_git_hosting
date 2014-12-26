7d4cf69711c233521e28eb389510f4d19b60a3e0
root <root@dev.jbox-web.fr>
2014-12-26 21:16:45 +0100

    Extract methods

--- lib/redmine_git_hosting/gitolite_wrapper/users.rb
+++ lib/redmine_git_hosting/gitolite_wrapper/users.rb
@@ -2,8 +2,24 @@ module RedmineGitHosting
   module GitoliteWrapper
     class Users < Admin
 
+      attr_reader :ssh_key
+      attr_reader :ssh_keys
+
+
+      def initialize(*args)
+        super
+
+        if object_id.is_a?(Hash)
+          @ssh_key = object_id.symbolize_keys
+        elsif object_id == 'all'
+          @ssh_keys = GitolitePublicKey.all
+        else
+          @ssh_key = GitolitePublicKey.find_by_id(object_id)
+        end
+      end
+
+
       def add_ssh_key
-        ssh_key = GitolitePublicKey.find_by_id(object_id)
         logger.info("Adding SSH key #{ssh_key.identifier}")
         admin.transaction do
           add_gitolite_key(ssh_key)
@@ -13,7 +29,6 @@ module RedmineGitHosting
 
 
       def delete_ssh_key
-        ssh_key = object_id.symbolize_keys
         logger.info("Deleting SSH key #{ssh_key[:identifier]}")
         admin.transaction do
           remove_gitolite_key(ssh_key)
@@ -23,7 +38,6 @@ module RedmineGitHosting
 
 
       def resync_all_ssh_keys
-        ssh_keys = GitolitePublicKey.all
         admin.transaction do
           ssh_keys.each do |ssh_key|
             add_gitolite_key(ssh_key)
@@ -37,31 +51,55 @@ module RedmineGitHosting
 
 
         def add_gitolite_key(key)
-          parts     = key.key.split
-          repo_keys = admin.ssh_keys[key.owner]
-          repo_key  = repo_keys.find_all{|k| k.location == key.location && k.owner == key.owner}.first
+          repo_key = keys_for_owner(key.owner).find_all{|k| k.location == key.location && k.owner == key.owner}.first
 
+          # Add it if not found
           unless repo_key
-            repo_key = Gitolite::SSHKey.new(parts[0], parts[1], parts[2], key.owner, key.location)
+            repo_key = build_gitolite_key(key)
             admin.add_key(repo_key)
           else
             logger.info("#{action} : SSH key '#{key.owner}@#{key.location}' already exists in Gitolite, update it ...")
-            repo_key.type, repo_key.blob, repo_key.email = parts
-            repo_key.owner = key.owner
+            repo_key.type     = key_type(key.key)
+            repo_key.blob     = key_blob(key.key)
+            repo_key.email    = key_email(key.key)
+            repo_key.owner    = key.owner
             repo_key.location = key.location
           end
         end
 
 
         def remove_gitolite_key(key)
-          repo_keys = admin.ssh_keys[key[:owner]]
-          repo_key  = repo_keys.find_all{|k| k.location == key[:location] && k.owner == key[:owner]}.first
-
+          repo_key = keys_for_owner(key[:owner]).find_all{|k| k.location == key[:location] && k.owner == key[:owner]}.first
           if repo_key
             admin.rm_key(repo_key)
           else
             logger.info("#{action} : SSH key '#{key[:owner]}@#{key[:location]}' does not exits in Gitolite, exit !")
           end
+        end
+
+
+        def build_gitolite_key(key)
+          Gitolite::SSHKey.new(key_type(key.key), key_blob(key.key), key_email(key.key), key.owner, key.location)
+        end
+
+
+        def keys_for_owner(owner)
+          admin.ssh_keys[owner]
+        end
+
+
+        def key_type(key)
+          key.split(' ')[0]
+        end
+
+
+        def key_blob(key)
+          key.split(' ')[1]
+        end
+
+
+        def key_email(key)
+          key.split(' ')[2]
         end
 
     end
