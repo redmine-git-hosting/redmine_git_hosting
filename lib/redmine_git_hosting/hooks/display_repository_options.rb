e1f59dbdf34cd4d5fc6cb091ba88653c0b184534
root <root@dev.jbox-web.fr>
2014-12-26 04:22:06 +0100

    Remove useless hook

--- lib/redmine_git_hosting/hooks/display_repository_options.rb
+++
@@ -1,7 +0,0 @@
-module RedmineGitHosting
-  module Hooks
-    class DisplayRepositoryOptions < Redmine::Hook::ViewListener
-      render_on :view_repository_form, :partial => 'repositories/git_form'
-    end
-  end
-end
