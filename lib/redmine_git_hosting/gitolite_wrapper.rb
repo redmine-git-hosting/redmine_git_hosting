148ffec2a7a373c7b98eeb026a7947a91954ff36
root <root@dev.jbox-web.fr>
2014-12-26 20:50:13 +0100

    Extract a little more

--- lib/redmine_git_hosting/gitolite_wrapper.rb
+++ lib/redmine_git_hosting/gitolite_wrapper.rb
@@ -61,18 +61,40 @@ module RedmineGitHosting
       #
       # action: An API action defined in one of the gitolite/* classes.
       def update(action, object, options = {})
+        # Symbolize keys before using them
         options = options.symbolize_keys
+
+        # Flush cache if needed
         flush_cache(options)
+
+        # Be sure to have a Gitolite::GitoliteAdmin object.
+        # Return nil if issues.
         begin
           admin = gitolite_admin
         rescue Rugged::SshError => e
           logger.error(e.message)
         else
-          WRAPPERS.each do |wrappermod|
-            return wrappermod.new(admin, action, object, options).send(action) if wrappermod.method_defined?(action)
-          end
+          # Call our wrapper passing the GitoliteAdmin object
+          call_gitolite_wrapper(admin, action, object, options)
+        end
+      end
+
+
+      def call_gitolite_wrapper(admin, action, object, options = {})
+        klass = find_gitolite_wrapper(action)
+        unless klass.nil?
+          klass.new(admin, action, object, options).send(action)
+        else
           raise RedmineGitHosting::Error::GitoliteWrapperException.new(action, "No available Wrapper for action '#{action}' found.")
         end
+      end
+
+
+      def find_gitolite_wrapper(action)
+        WRAPPERS.each do |wrappermod|
+          return wrappermod if wrappermod.method_defined?(action)
+        end
+        return nil
       end
 
     end
