fb7612367fe56af9bb64ecb25c21ba9e02daabdc
root <root@dev.jbox-web.fr>
2014-12-28 02:32:33 +0100

    DRY ApplySettings

--- app/use_cases/apply_settings.rb
+++ app/use_cases/apply_settings.rb
@@ -44,27 +44,28 @@ class ApplySettings
     end
 
 
+    def value_has_changed?(params)
+      old_valuehash[params] != valuehash[params]
+    end
+
+
     def check_repo_hierarchy
       ## Storage infos has changed, move repositories!
-      if old_valuehash[:gitolite_global_storage_dir]  != valuehash[:gitolite_global_storage_dir]  ||
-         old_valuehash[:gitolite_redmine_storage_dir] != valuehash[:gitolite_redmine_storage_dir] ||
-         old_valuehash[:hierarchical_organisation]    != valuehash[:hierarchical_organisation]
+      if value_has_changed?(:gitolite_global_storage_dir)  ||
+         value_has_changed?(:gitolite_redmine_storage_dir) ||
+         value_has_changed?(:hierarchical_organisation)
 
         # Need to update everyone!
         # We take all root projects (even those who are closed) and move each hierarchy individually
         count = Project.includes(:repositories).all.select { |x| x if x.parent_id.nil? }.length
-        if count > 0
-          MoveRepositoriesTree.new(count).call
-        end
+        MoveRepositoriesTree.new(count).call if count > 0
       end
     end
 
 
     def check_gitolite_config
       ## Gitolite config file has changed, create a new one!
-      if old_valuehash[:gitolite_config_file] != valuehash[:gitolite_config_file] ||
-         old_valuehash[:gitolite_config_has_admin_key] != valuehash[:gitolite_config_has_admin_key]
-
+      if value_has_changed?(:gitolite_config_file) || value_has_changed?(:gitolite_config_has_admin_key)
         options = { message: "Gitolite configuration has been modified, resync all projects (active, closed, archived)..." }
         UpdateProjects.new('all', options).call
       end
@@ -73,30 +74,23 @@ class ApplySettings
 
     def check_gitolite_default_values
       ## Gitolite default values has changed, update active projects
-      if old_valuehash[:gitolite_notify_global_prefix]         != valuehash[:gitolite_notify_global_prefix]         ||
-         old_valuehash[:gitolite_notify_global_sender_address] != valuehash[:gitolite_notify_global_sender_address] ||
-         old_valuehash[:gitolite_notify_global_include]        != valuehash[:gitolite_notify_global_include]        ||
-         old_valuehash[:gitolite_notify_global_exclude]        != valuehash[:gitolite_notify_global_exclude]
+      if value_has_changed?(:gitolite_notify_global_prefix)         ||
+         value_has_changed?(:gitolite_notify_global_sender_address) ||
+         value_has_changed?(:gitolite_notify_global_include)        ||
+         value_has_changed?(:gitolite_notify_global_exclude)
 
+        # Need to update everyone!
         options = { message: "Gitolite configuration has been modified, resync all active projects..." }
         UpdateProjects.new('active', options).call
       end
     end
 
 
-    def check_hook_install
-      ## Gitolite user has changed, check if this new one has our hooks!
-      if old_valuehash[:gitolite_user] != valuehash[:gitolite_user]
-        RedmineGitHosting::HookManager.check_install!
-      end
-    end
-
-
     def check_hook_config
       ## Gitolite hooks config has changed, update our .gitconfig!
-      if old_valuehash[:gitolite_hooks_debug]            != valuehash[:gitolite_hooks_debug]        ||
-         old_valuehash[:gitolite_force_hooks_update]     != valuehash[:gitolite_force_hooks_update] ||
-         old_valuehash[:gitolite_hooks_are_asynchronous] != valuehash[:gitolite_hooks_are_asynchronous]
+      if value_has_changed?(:gitolite_hooks_debug)        ||
+         value_has_changed?(:gitolite_force_hooks_update) ||
+         value_has_changed?(:gitolite_hooks_are_asynchronous)
 
         # Need to update our .gitconfig
         RedmineGitHosting::HookManager.update_hook_params!
@@ -104,43 +98,38 @@ class ApplySettings
     end
 
 
+    def check_hook_install
+      ## Gitolite user has changed, check if this new one has our hooks!
+      RedmineGitHosting::HookManager.check_install! if value_has_changed?(:gitolite_user)
+    end
+
+
     def check_cache_config
       ## Gitolite cache has changed, clear cache entries!
-      if old_valuehash[:gitolite_cache_max_time] != valuehash[:gitolite_cache_max_time]
-        RedmineGitHosting::CacheManager.clear_obsolete_cache_entries
-      end
+      RedmineGitHosting::CacheManager.clear_obsolete_cache_entries if value_has_changed?(:gitolite_cache_max_time)
     end
 
 
     def do_resync_projects
       ## A resync has been asked within the interface, update all projects in force mode
-      if resync_projects
-        options = { message: "Forced resync of all projects (active, closed, archived)...", force: true }
-        UpdateProjects.new('all', options).call
-      end
+      UpdateProjects.new('all', { message: "Forced resync of all projects (active, closed, archived)...", force: true }).call if resync_projects
     end
 
 
     def do_resync_ssh_keys
       ## A resync has been asked within the interface, update all projects in force mode
-      if resync_ssh_keys
-        ResyncSshKeys.new().call
-      end
+      ResyncSshKeys.new().call if resync_ssh_keys
     end
 
 
     def do_flush_cache
       ## A cache flush has been asked within the interface
-      if flush_cache
-        FlushGitCache.new().call
-      end
+      FlushGitCache.new().call if flush_cache
     end
 
 
     def do_delete_trash_repo
-      if !delete_trash_repo.empty?
-        PurgeRecycleBin.new(delete_trash_repo).call
-      end
+      PurgeRecycleBin.new(delete_trash_repo).call if !delete_trash_repo.empty?
     end
 
 end
