a450cec8448369536d4b5966992cf8dace7008eb
root <root@dev.jbox-web.fr>
2014-12-30 00:16:54 +0100

    Fix RepositoryExtra initialization

--- app/use_cases/create_repository.rb
+++ app/use_cases/create_repository.rb
@@ -15,12 +15,53 @@ class CreateRepository
 
 
   def call
+    set_repository_extras
     create_repository
     super
   end
 
 
   private
+
+
+    def set_repository_extras
+      extra = repository.build_git_extra(default_extra_options)
+      extra.save!
+    end
+
+
+    def default_extra_options
+      enable_git_annex? ? git_annex_repository_options : standard_repository_options
+    end
+
+
+    def enable_git_annex?
+      options[:enable_git_annex]
+    end
+
+
+    def standard_repository_options
+      {
+        git_http:       RedmineGitHosting::Config.get_setting(:gitolite_http_by_default),
+        git_daemon:     RedmineGitHosting::Config.get_setting(:gitolite_daemon_by_default, true),
+        git_notify:     RedmineGitHosting::Config.get_setting(:gitolite_notify_by_default, true),
+        git_annex:      false,
+        default_branch: 'master',
+        key:            RedmineGitHosting::Utils.generate_secret(64)
+      }
+    end
+
+
+    def git_annex_repository_options
+      {
+        git_http:       0,
+        git_daemon:     false,
+        git_notify:     false,
+        git_annex:      true,
+        default_branch: 'git-annex',
+        key:            RedmineGitHosting::Utils.generate_secret(64)
+      }
+    end
 
 
     def create_repository
--- lib/redmine_git_hosting/patches/repositories_controller_patch.rb
+++ lib/redmine_git_hosting/patches/repositories_controller_patch.rb
@@ -69,7 +69,6 @@ module RedmineGitHosting
               if !@repository.errors.any?
                 case self.action_name
                 when 'create'
-                  set_repository_extras
                   CreateRepository.new(@repository, creation_options).call
                 when 'update'
                   UpdateRepository.new(@repository).call
@@ -78,12 +77,6 @@ module RedmineGitHosting
                 end
               end
             end
-          end
-
-
-          def set_repository_extras
-            extra = @repository.build_git_extra(default_extra_options)
-            extra.save!
           end
 
 
@@ -104,35 +97,6 @@ module RedmineGitHosting
 
           def destroy_options
             {message: "User '#{User.current.login}' has removed repository '#{@repository.gitolite_repository_name}'"}
-          end
-
-
-          def default_extra_options
-            enable_git_annex? ? git_annex_repository_options : standard_repository_options
-          end
-
-
-          def standard_repository_options
-            {
-              git_http:       RedmineGitHosting::Config.get_setting(:gitolite_http_by_default),
-              git_daemon:     RedmineGitHosting::Config.get_setting(:gitolite_daemon_by_default, true),
-              git_notify:     RedmineGitHosting::Config.get_setting(:gitolite_notify_by_default, true),
-              git_annex:      false,
-              default_branch: 'master',
-              key:            RedmineGitHosting::Utils.generate_secret(64)
-            }
-          end
-
-
-          def git_annex_repository_options
-            {
-              git_http:       0,
-              git_daemon:     false,
-              git_notify:     false,
-              git_annex:      true,
-              default_branch: 'git-annex',
-              key:            RedmineGitHosting::Utils.generate_secret(64)
-            }
           end
 
       end
