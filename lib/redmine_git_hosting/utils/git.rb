7ab4c20c9045c3a619a14b3ecfc52f990849253f
root <root@dev.jbox-web.fr>
2015-01-03 07:14:22 +0100

    Indent, coding style

--- lib/redmine_git_hosting/utils/git.rb
+++ lib/redmine_git_hosting/utils/git.rb
@@ -16,20 +16,22 @@ module RedmineGitHosting::Utils
       # 2) name
       #
       # here, name can have many components.
+
       @@refcomp = "[\\.\\-\\w_\\*]+"
+
       def refcomp_parse(spec)
         if (refcomp_parse = spec.match(/^(refs\/)?((#{@@refcomp})\/)?(#{@@refcomp}(\/#{@@refcomp})*)$/))
           if refcomp_parse[1]
             # Should be first class.  If no type component, return fail
             if refcomp_parse[3]
-              {type: refcomp_parse[3], name: refcomp_parse[4]}
+              { type: refcomp_parse[3], name: refcomp_parse[4] }
             else
               nil
             end
           elsif refcomp_parse[3]
-            {type: nil, name: (refcomp_parse[3] + "/" + refcomp_parse[4])}
+            { type: nil, name: "#{refcomp_parse[3]}/#{refcomp_parse[4]}" }
           else
-            {type: nil, name: refcomp_parse[4]}
+            { type: nil, name: refcomp_parse[4] }
           end
         else
           nil
