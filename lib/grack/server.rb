f09a266c9b744c38814efb8a64a6d89ee8dc7b3d
root <root@dev.jbox-web.fr>
2015-01-04 00:58:43 +0100

    Fix IO.popen.read let zombie process behind him

--- lib/grack/server.rb
+++ lib/grack/server.rb
@@ -1,8 +1,10 @@
 module Grack
   class Server
 
+    #
     # Override original *get_git_dir* method because the path is relative
     # and accessed via Sudo.
+    #
     def get_git_dir(path)
       path = gitolite_path(path)
       if !directory_exists?(path)
@@ -13,26 +15,47 @@ module Grack
     end
 
 
+    #
     # Override original *git_command* method to prefix the command with Sudo and other args.
+    #
     def git_command(params)
       git_command_with_sudo(params)
     end
 
 
-    # Override original *capture* method to catch errors
+    #
+    # Override original *capture* method because the original IO.popen().read let zombie process behind.
+    #
+    # This method is called :
+    #  * to get repository Git config (http.uploadpack || http.receivepack)
+    #  * to get repository info refs :
+    #    0087deab8f3d612a47e7e153ed21bbc52a480205035a refs/heads/devel report-status delete-refs side-band-64k quiet ofs-delta agent=git/1.9.1
+    #  * to get repository refs :
+    #    003f91a7b1dad21020e96d52119c585881c02f2fae45 refs/heads/master
+    #
+    # Note : *service_rpc* also calls IO.popen but pass a block !.
+    # Passing a block to IO.popen auto-close the pipe/thread.
+    #
     def capture(command)
+      # Extract Args
+      cmd  = command.shift
+      args = command
+
       begin
-        IO.popen(popen_env, command, popen_options).read
+        RedmineGitHosting::Utils.capture(cmd, args)
       rescue => e
         logger.error('Problems while getting SmartHttp params')
       end
     end
 
 
+    #
     # Override original *popen_options* method.
     # The original one try to chdir before executing the command by
     # passing 'chdir: @dir' option to IO.popen.
     # This is wrong as we can't chdir to Gitolite directory.
+    # Notes : this method is called in *service_rpc* (not overriden)
+    #
     def popen_options
       { unsetenv_others: true }
     end
@@ -40,6 +63,8 @@ module Grack
 
     # Override original *popen_options* method.
     # The original one passes useless arg (GL_ID) to IO.popen.
+    # Notes : this method is called in *service_rpc* (not overriden)
+    #
     def popen_env
        { 'PATH' => ENV['PATH'] }
     end
