49f9baa256c425660c132183a836289b0229a2bf
root <root@dev.jbox-web.fr>
2015-01-03 07:33:04 +0100

    Coding style

--- app/helpers/extend_repositories_helper.rb
+++ app/helpers/extend_repositories_helper.rb
@@ -12,25 +12,25 @@ module ExtendRepositoriesHelper
 
   def report_last_commit_field(form, repository)
     content_tag(:p) do
-      form.check_box(:extra_report_last_commit, :label => l(:label_git_report_last_commit))
+      form.check_box(:extra_report_last_commit, label: l(:label_git_report_last_commit))
     end
   end
 
 
   def create_readme_field(form, repository)
     content_tag(:p) do
-      hidden_field_tag("repository[create_readme]", "false") +
+      hidden_field_tag('repository[create_readme]', 'false') +
       content_tag(:label, l(:label_init_repo_with_readme)) +
-      check_box_tag("repository[create_readme]", "true", RedmineGitHosting::Config.init_repositories_on_create?)
+      check_box_tag('repository[create_readme]', 'true', RedmineGitHosting::Config.init_repositories_on_create?)
     end if repository.new_record?
   end
 
 
   def enable_git_annex_field(form, repository)
     content_tag(:p) do
-      hidden_field_tag("repository[enable_git_annex]", "false") +
+      hidden_field_tag('repository[enable_git_annex]', 'false') +
       content_tag(:label, l(:label_init_repo_with_git_annex)) +
-      check_box_tag("repository[enable_git_annex]", "true")
+      check_box_tag('repository[enable_git_annex]', 'true')
     end if repository.new_record?
   end
 
