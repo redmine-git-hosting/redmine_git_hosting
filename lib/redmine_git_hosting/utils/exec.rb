e48535784f41fc764538af08ee9b0cf46bbec1a4
root <root@dev.jbox-web.fr>
2015-01-04 01:02:14 +0100

    Allow to pass a block to Open3.capture or Open3.execute

--- lib/redmine_git_hosting/utils/exec.rb
+++ lib/redmine_git_hosting/utils/exec.rb
@@ -17,9 +17,9 @@ module RedmineGitHosting::Utils
       #
       # If the operation throws an exception or the operation yields a non-zero exit code
       # we rethrow a +GitoliteCommandException+ with a meaningful error message.
-      def capture(command, args = [], opts = {})
+      def capture(command, args = [], opts = {}, &block)
         merge_output = opts.delete(:merge_output){ false }
-        stdout, stderr, code = execute(command, args, opts)
+        stdout, stderr, code = execute(command, args, opts, &block)
         if code != 0
           error_msg = "Non-zero exit code #{code} for `#{command} #{args.join(" ")}`"
           RedmineGitHosting.logger.debug(error_msg)
@@ -35,8 +35,8 @@ module RedmineGitHosting::Utils
       #
       # If the operation throws an exception or the operation we rethrow a
       # +GitoliteCommandException+ with a meaningful error message.
-      def execute(command, args = [], opts = {})
-        Open3.capture3(command, *args, opts)
+      def execute(command, args = [], opts = {}, &block)
+        Open3.capture3(command, *args, opts, &block)
       rescue => e
         error_msg = "Exception occured executing `#{command} #{args.join(" ")}` : #{e.message}"
         RedmineGitHosting.logger.debug(error_msg)
