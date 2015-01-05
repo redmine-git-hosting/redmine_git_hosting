03e7deb8d6b6577766ea6be9b87e338442e2b75e
root <root@dev.jbox-web.fr>
2015-01-06 00:27:10 +0100

    Avoid reloading of Redmine Hooks in dev environment

--- lib/redmine_git_hosting.rb
+++ lib/redmine_git_hosting.rb
@@ -1,13 +1,24 @@
 ## Redmine SCM adapter
 require 'redmine/scm/adapters/xitolite_adapter'
 
+## Redmine Views Hooks
+require 'redmine_git_hosting/hooks/add_plugin_icon'
+require 'redmine_git_hosting/hooks/add_public_keys_link'
+require 'redmine_git_hosting/hooks/display_git_urls_on_project'
+require 'redmine_git_hosting/hooks/display_git_urls_on_repository_edit'
+require 'redmine_git_hosting/hooks/display_repository_extras'
+require 'redmine_git_hosting/hooks/display_repository_readme'
+require 'redmine_git_hosting/hooks/display_repository_sidebar'
+
+
 # Set up autoload of patches
 Rails.configuration.to_prepare do
 
   ## Redmine Git Hosting Libs, Patches and Hooks
   rbfiles = Rails.root.join('plugins', 'redmine_git_hosting', 'lib', 'redmine_git_hosting', '**', '*.rb')
   Dir.glob(rbfiles).each do |file|
-    require_dependency file
+    # Exclude Redmine Views Hooks from Rails loader to avoid multiple calls to hooks on reload in dev environment.
+    require_dependency file unless File.dirname(file) == Rails.root.join('plugins', 'redmine_git_hosting', 'lib', 'redmine_git_hosting', 'hooks').to_s
   end
 
   require_dependency 'grack/auth'
