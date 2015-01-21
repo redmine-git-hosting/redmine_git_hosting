4b945a24bf802eac4a1a7949a83179ae7f9bdc6e
root <root@dev.jbox-web.fr>
2015-01-21 06:11:15 +0100

    Fix migrations rollback

--- db/migrate/20140417004100_enforce_models_constraints.rb
+++ db/migrate/20140417004100_enforce_models_constraints.rb
@@ -8,4 +8,11 @@ class EnforceModelsConstraints < ActiveRecord::Migration
     remove_column :repository_post_receive_urls, :updated_at
   end
 
+  def self.down
+    add_column :repository_mirrors, :created_at, :datetime
+    add_column :repository_mirrors, :updated_at, :datetime
+    add_column :repository_post_receive_urls, :created_at, :datetime
+    add_column :repository_post_receive_urls, :updated_at, :datetime
+  end
+
 end
