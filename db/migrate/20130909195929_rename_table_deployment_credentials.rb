24e65510e586f5feaed02b22b9651bde67f54002
root <root@dev.jbox-web.fr>
2015-02-24 23:09:34 +0100

    Remove indexes before renaming table

--- db/migrate/20130909195929_rename_table_deployment_credentials.rb
+++ db/migrate/20130909195929_rename_table_deployment_credentials.rb
@@ -1,11 +1,13 @@
 class RenameTableDeploymentCredentials < ActiveRecord::Migration
 
   def self.up
+    remove_index :deployment_credentials, :gitolite_public_key_id
     rename_table :deployment_credentials, :repository_deployment_credentials
   end
 
   def self.down
     rename_table :repository_deployment_credentials, :deployment_credentials
+    add_index :deployment_credentials, :gitolite_public_key_id
   end
 
 end
