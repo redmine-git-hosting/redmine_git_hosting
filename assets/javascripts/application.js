c3324a69ff6251d088477d7de0406ffdd67dca1e
root <root@dev.jbox-web.fr>
2015-03-14 13:04:53 +0100

    Use RedmineBootstrapKit helper method to load JS and CSS, move JS in application.js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -27,3 +27,28 @@ function key_mode_change(elem) {
     $('#new_key_window').hide();
   }
 }
+
+function updateUrl(element) {
+  var url = $(element).data('url');
+  var target = $(element).data('target');
+  var committer = $(element).data('committer');
+  $('#git_url_text_' + target).val(url);
+  $('#git_url_permissions_' + target).html(committer);
+  $(element).parent().find('li').removeClass('selected');
+  $(element).addClass('selected');
+}
+
+function setFirstGitUrl(elements) {
+  $(elements).each(function(index, element){
+    var first_url = $(element).children().first();
+    updateUrl(first_url);
+  });
+}
+
+function bindGitUrls(elements) {
+  $(elements).each(function(index, element){
+    $(element).on('click', function(){
+      updateUrl($(this));
+    });
+  });
+}
