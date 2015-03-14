b53b5d511ba6a3c82cc0483318840a97b027dd7f
root <root@dev.jbox-web.fr>
2015-03-14 01:02:11 +0100

    Add unique index on fingerprint

---
+++ db/migrate/20150314005600_add_unique_index_on_fingerprint.rb
@@ -0,0 +1,11 @@
+class AddUniqueIndexOnFingerprint < ActiveRecord::Migration
+
+  def self.up
+    add_index :gitolite_public_keys, :fingerprint, unique: true
+  end
+
+  def self.down
+    remove_index :gitolite_public_keys, :fingerprint
+  end
+
+end
