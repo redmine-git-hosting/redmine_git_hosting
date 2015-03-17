57d414aaa5e40438c1cceb4670524b2bad9800fe
root <root@dev.jbox-web.fr>
2015-03-17 22:12:44 +0100

    Add comments

--- app/models/concerns/gitolitable/permissions.rb
+++ app/models/concerns/gitolitable/permissions.rb
@@ -2,6 +2,8 @@ module Gitolitable
   module Permissions
     extend ActiveSupport::Concern
 
+    # These are for repository Gitolite configuration
+
     def git_daemon_available?
       User.anonymous.allowed_to?(:view_changesets, project) && git_daemon_enabled?
     end
@@ -9,6 +11,33 @@ module Gitolitable
 
     def git_web_available?
       User.anonymous.allowed_to?(:browse_repository, project) && smart_http_enabled?
+    end
+
+
+    def protected_branches_available?
+      protected_branches_enabled? && project.active? && protected_branches.any?
+    end
+
+
+    def clonable_via_http?
+      User.anonymous.allowed_to?(:view_changesets, project) || smart_http_enabled?
+    end
+
+
+    def pushable_via_http?
+      https_access_enabled?
+    end
+
+
+    def git_notification_available?
+      git_notification_enabled? && !mailing_list.empty?
+    end
+
+
+    # These are for repository URLs
+
+    def urls_are_viewable?
+      RedmineGitHosting::Config.show_repositories_url? && User.current.allowed_to?(:view_changesets, project)
     end
 
 
@@ -42,33 +71,8 @@ module Gitolitable
     end
 
 
-    def protected_branches_available?
-      protected_branches_enabled? && project.active? && protected_branches.any?
-    end
-
-
-    def urls_are_viewable?
-      RedmineGitHosting::Config.show_repositories_url? && User.current.allowed_to?(:view_changesets, project)
-    end
-
-
-    def clonable_via_http?
-      User.anonymous.allowed_to?(:view_changesets, project) || smart_http_enabled?
-    end
-
-
-    def pushable_via_http?
-      https_access_enabled?
-    end
-
-
     def downloadable?
       git_annex_enabled? ? false : User.current.allowed_to_download?(self)
-    end
-
-
-    def git_notification_available?
-      git_notification_enabled? && !mailing_list.empty?
     end
 
 
