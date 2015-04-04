7842ff8f84d36f34cc0e49048df4bb6d1f65ff09
root <root@dev.jbox-web.fr>
2015-04-04 03:27:26 +0200

    Add test for ProtectBranche uniqueness

--- spec/models/repository_protected_branche_spec.rb
+++ spec/models/repository_protected_branche_spec.rb
@@ -31,6 +31,8 @@ describe RepositoryProtectedBranche do
     it { should validate_presence_of(:permissions) }
     it { should validate_presence_of(:user_list) }
 
+    it { should validate_uniqueness_of(:path).scoped_to([:permissions, :repository_id]) }
+
     it { should validate_inclusion_of(:permissions).in_array(%w(RW RW+)) }
 
     ## Serializations
