1ee682c53596b1eac0f38f20753e6f9f76c05ef3
root <root@dev.jbox-web.fr>
2015-06-23 23:07:52 +0200

    Rework Travis installer

--- contrib/travis/plugin.sh
+++ contrib/travis/plugin.sh
@@ -3,10 +3,98 @@
 GITHUB_USER=${GITHUB_USER:-jbox-web}
 GITHUB_PROJECT=${GITHUB_PROJECT:-redmine_git_hosting}
 
-function install_plugin_packages() {
+GITHUB_SOURCE="${GITHUB_USER}/${GITHUB_PROJECT}"
+PLUGIN_PATH=${PLUGIN_PATH:-$GITHUB_SOURCE}
+PLUGIN_NAME=${PLUGIN_NAME:-$GITHUB_PROJECT}
+PLUGIN_DIR="redmine/plugins/${PLUGIN_NAME}"
+CONTRIB_DATA_DIR="${PLUGIN_DIR}/contrib/travis/data"
+
+function install_plugin() {
+  install_plugin_libs
+  move_plugin
+  install_database
+  install_gemfile
+  install_rspec
+  install_plugin_dependencies
+}
+
+
+## PRIVATE
+
+
+function install_plugin_libs() {
   log_title "INSTALL ADDITIONAL PACKAGES"
-  sudo apt-get update -qq
-  sudo apt-get install -qq libicu-dev libssh2-1 libssh2-1-dev cmake subversion
+  sudo apt-get install -qq libicu-dev libssh2-1 libssh2-1-dev cmake
+}
+
+
+function move_plugin() {
+  log_title "MOVE PLUGIN"
+  # Move GITHUB_USER/GITHUB_PROJECT to redmine/plugins dir
+  mv "${PLUGIN_PATH}" "${REDMINE_NAME}/plugins"
+  # Remove parent dir (GITHUB_USER)
+  rmdir $(dirname ${PLUGIN_PATH})
+  log_ok
+
+  log_title "CREATE SYMLINK"
+  ln -s "${REDMINE_NAME}" "redmine"
+  log_ok
+}
+
+
+function install_database() {
+  log_title "INSTALL DATABASE FILE"
+  if [ "$DATABASE_ADAPTER" == "mysql" ] ; then
+    echo "Type : mysql"
+    cp "${CONTRIB_DATA_DIR}/db_files/database_mysql.yml" "redmine/config/database.yml"
+  else
+    echo "Type : postgres"
+    cp "${CONTRIB_DATA_DIR}/db_files/database_postgres.yml" "redmine/config/database.yml"
+  fi
+
+  log_ok
+}
+
+
+function install_gemfile() {
+  log_title "INSTALL GEMFILE"
+
+  if [ "$major" == "3" ] ; then
+    if [ -f "${CONTRIB_DATA_DIR}/gem_files/rails4.gemfile" ] ; then
+      log_title "INSTALL RAILS 4 VERSION"
+      cp "${CONTRIB_DATA_DIR}/gem_files/rails4.gemfile" "${PLUGIN_DIR}/Gemfile"
+      log_ok
+    fi
+  else
+
+    if [ -f "${CONTRIB_DATA_DIR}/gem_files/rails3.gemfile" ] ; then
+      log_title "INSTALL RAILS 3 VERSION"
+      cp "${CONTRIB_DATA_DIR}/gem_files/rails3.gemfile" "${PLUGIN_DIR}/Gemfile"
+      log_ok
+    fi
+
+    log_title "RAILS 3 : UPDATE REDMINE GEMFILE"
+
+    echo "Update shoulda to 3.5.0"
+    sed -i 's/gem "shoulda", "~> 3.3.2"/gem "shoulda", "~> 3.5.0"/' "redmine/Gemfile"
+    log_ok
+
+    echo "Let update shoulda-matchers to 2.7.0"
+    sed -i 's/gem "shoulda-matchers", "1.4.1"/#gem "shoulda-matchers", "1.4.1"/' "redmine/Gemfile"
+    log_ok
+
+    echo "Update capybara to 2.2.0"
+    sed -i 's/gem "capybara", "~> 2.1.0"/gem "capybara", "~> 2.2.0"/' "redmine/Gemfile"
+    log_ok
+  fi
+}
+
+
+function install_rspec() {
+  log_title "INSTALL RSPEC FILE"
+  mkdir "redmine/spec"
+  cp "${PLUGIN_DIR}/spec/root_spec_helper.rb" "redmine/spec/spec_helper.rb"
+  log_ok
 }
 
 
@@ -20,7 +108,7 @@ function install_plugin_dependencies() {
 
 function install_ssh_key() {
   log_title "INSTALL ADMIN SSH KEY"
-  ssh-keygen -N '' -f "redmine/plugins/${PLUGIN_NAME}/ssh_keys/redmine_gitolite_admin_id_rsa"
+  ssh-keygen -N '' -f "${PLUGIN_DIR}/ssh_keys/redmine_gitolite_admin_id_rsa"
   log_ok
 }
 
@@ -32,7 +120,7 @@ function install_gitolite() {
   sudo -n -u git -i git clone https://github.com/sitaramc/gitolite.git
   sudo -n -u git -i mkdir bin
   sudo -n -u git -i gitolite/install -to /home/git/bin
-  sudo cp "redmine/plugins/${PLUGIN_NAME}/ssh_keys/redmine_gitolite_admin_id_rsa.pub" /home/git/
+  sudo cp "${PLUGIN_DIR}/ssh_keys/redmine_gitolite_admin_id_rsa.pub" /home/git/
   sudo chown git.git /home/git/redmine_gitolite_admin_id_rsa.pub
   sudo -n -u git -i gitolite setup -pk redmine_gitolite_admin_id_rsa.pub
 
--- contrib/travis/redmine.sh
+++ contrib/travis/redmine.sh
@@ -14,9 +14,35 @@ major=${version[0]}
 minor=${version[1]}
 patch=${version[2]}
 
-GITHUB_SOURCE="${GITHUB_USER}/${GITHUB_PROJECT}"
-PLUGIN_PATH=${PLUGIN_PATH:-$GITHUB_SOURCE}
-PLUGIN_NAME=${PLUGIN_NAME:-$GITHUB_PROJECT}
+
+function install_redmine() {
+  install_redmine_libs
+
+  if [ $USE_SVN == 'true' ] ; then
+    install_redmine_from_svn
+  else
+    install_redmine_from_package
+  fi
+}
+
+function finish_install() {
+  log_header "CURRENT DIRECTORY LISTING"
+  ls -l "${CURRENT_DIR}"
+  echo ""
+
+  log_header "REDMINE PLUGIN DIRECTORY LISTING"
+  ls -l "${REDMINE_NAME}/plugins"
+  echo ""
+}
+
+## PRIVATE
+
+
+function install_redmine_libs() {
+  log_title "INSTALL REDMINE LIBS"
+  sudo apt-get install -qq subversion
+  log_ok
+}
 
 
 function install_redmine_from_package() {
@@ -34,87 +60,4 @@ function install_redmine_from_svn() {
   log_title "GET SOURCES FROM SVN"
   svn co --non-interactive --trust-server-cert "${REDMINE_SVN_URL}/${REDMINE_VERSION}" "${REDMINE_NAME}"
   log_ok
-}
-
-
-function install_redmine() {
-  if [ $USE_SVN == 'true' ] ; then
-    install_redmine_from_svn
-  else
-    install_redmine_from_package
-  fi
-}
-
-
-function install_plugin() {
-  log_title "MOVE PLUGIN"
-  # Move GITHUB_USER/GITHUB_PROJECT to redmine/plugins dir
-  mv "${PLUGIN_PATH}" "${REDMINE_NAME}/plugins"
-  # Remove parent dir (GITHUB_USER)
-  rmdir $(dirname ${PLUGIN_PATH})
-  log_ok
-
-  log_title "CREATE SYMLINK"
-  ln -s "${REDMINE_NAME}" "redmine"
-  ln -s "redmine/plugins/redmine_git_hosting/.git" "${REDMINE_NAME}/.git"
-  log_ok
-
-  log_title "INSTALL DATABASE FILE"
-  if [ "$DATABASE_ADAPTER" == "mysql" ] ; then
-    echo "Type : mysql"
-    cp "redmine/plugins/${PLUGIN_NAME}/spec/database_mysql.yml" "redmine/config/database.yml"
-  else
-    echo "Type : postgres"
-    cp "redmine/plugins/${PLUGIN_NAME}/spec/database_postgres.yml" "redmine/config/database.yml"
-  fi
-
-  log_ok
-}
-
-
-function install_rspec() {
-  log_title "INSTALL RSPEC FILE"
-  mkdir "redmine/spec"
-  cp "redmine/plugins/${PLUGIN_NAME}/spec/root_spec_helper.rb" "redmine/spec/spec_helper.rb"
-  log_ok
-
-  if [ "$major" == "3" ] ; then
-    if [ -f "redmine/plugins/${PLUGIN_NAME}/gemfiles/rails4.gemfile" ] ; then
-      log_title "RAILS 4 : INSTALL GEMFILE"
-      cp "redmine/plugins/${PLUGIN_NAME}/gemfiles/rails4.gemfile" "redmine/plugins/${PLUGIN_NAME}/Gemfile"
-      log_ok
-    fi
-  else
-
-    if [ -f "redmine/plugins/${PLUGIN_NAME}/gemfiles/rails3.gemfile" ] ; then
-      log_title "RAILS 3 : INSTALL GEMFILE"
-      cp "redmine/plugins/${PLUGIN_NAME}/gemfiles/rails3.gemfile" "redmine/plugins/${PLUGIN_NAME}/Gemfile"
-      log_ok
-    fi
-
-    log_title "RAILS 3 : UPDATE REDMINE GEMFILE"
-
-    echo "Update shoulda to 3.5.0"
-    sed -i 's/gem "shoulda", "~> 3.3.2"/gem "shoulda", "~> 3.5.0"/' "redmine/Gemfile"
-    log_ok
-
-    echo "Let update shoulda-matchers to 2.7.0"
-    sed -i 's/gem "shoulda-matchers", "1.4.1"/#gem "shoulda-matchers", "1.4.1"/' "redmine/Gemfile"
-    log_ok
-
-    echo "Update capybara to 2.2.0"
-    sed -i 's/gem "capybara", "~> 2.1.0"/gem "capybara", "~> 2.2.0"/' "redmine/Gemfile"
-    log_ok
-  fi
-}
-
-
-function finish_install() {
-  log_header "CURRENT DIRECTORY LISTING"
-  ls -l "${CURRENT_DIR}"
-  echo ""
-
-  log_header "REDMINE PLUGIN DIRECTORY LISTING"
-  ls -l "${REDMINE_NAME}/plugins"
-  echo ""
 }
