552199092901c3954d5324250a0eadbeda2c29f7
root <root@dev.jbox-web.fr>
2015-07-08 03:20:46 +0200

    Minor change

--- lib/redmine_git_hosting/plugins.rb
+++ lib/redmine_git_hosting/plugins.rb
@@ -1,18 +1,15 @@
 module RedmineGitHosting
   module Plugins
+    extend self
 
-    class << self
-
-      def execute(step, repository, opts = {})
-        begin
-          Plugins::GitolitePlugin.all_plugins.each do |plugin|
-            plugin.new(repository, opts).send(step) if plugin.method_defined?(step)
-          end
-        rescue => e
-          RedmineGitHosting.logger.error e.message
+    def execute(step, repository, opts = {})
+      begin
+        Plugins::GitolitePlugin.all_plugins.each do |plugin|
+          plugin.new(repository, opts).send(step) if plugin.method_defined?(step)
         end
+      rescue => e
+        RedmineGitHosting.logger.error e.message
       end
-
     end
 
   end
