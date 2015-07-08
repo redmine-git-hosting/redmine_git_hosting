168da0ae02e85999a9eb9417ad48eed562bd3d5d
root <root@dev.jbox-web.fr>
2015-07-08 03:20:34 +0200

    Fix namespace

--- lib/redmine_git_hosting/console_logger.rb
+++ lib/redmine_git_hosting/console_logger.rb
@@ -1,26 +1,28 @@
-class ConsoleLogger
+module RedmineGitHosting
+  class ConsoleLogger
 
-  attr_reader :console
-  attr_reader :logger
+    attr_reader :console
+    attr_reader :logger
 
-  def initialize(opts = {})
-    @console = opts[:console] || false
-    @logger ||= RedmineGitHosting.logger
+    def initialize(opts = {})
+      @console = opts[:console] || false
+      @logger ||= RedmineGitHosting.logger
+    end
+
+    def info(message)
+      puts message if console
+      logger.info(message)
+    end
+
+    def error(message)
+      puts message if console
+      logger.error(message)
+    end
+
+    # Handle everything else with base object
+    def method_missing(m, *args, &block)
+      logger.send m, *args, &block
+    end
+
   end
-
-  def info(message)
-    puts message if console
-    logger.info(message)
-  end
-
-  def error(message)
-    puts message if console
-    logger.error(message)
-  end
-
-  # Handle everything else with base object
-  def method_missing(m, *args, &block)
-    logger.send m, *args, &block
-  end
-
 end
