c03f567b20ba8c4a88ea7560db95c9aa3b2c66d5
root <root@dev.jbox-web.fr>
2015-08-26 09:42:27 +0200

    Fix https://github.com/jbox-web/redmine_git_hosting/issues/472

--- lib/redmine_git_hosting/hooks/add_public_keys_link.rb
+++ lib/redmine_git_hosting/hooks/add_public_keys_link.rb
@@ -7,6 +7,10 @@ module RedmineGitHosting
         link_to(l(:label_my_public_keys), public_keys_path, class: 'icon icon-passwd') if user.allowed_to_create_ssh_keys?
       end
 
+      def self.default_url_options
+        {}
+      end
+
     end
   end
 end
