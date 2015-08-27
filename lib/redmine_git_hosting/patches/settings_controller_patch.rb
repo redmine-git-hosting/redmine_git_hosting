a0e2c7131f8a7483433452d32e505e2310d94823
root <root@dev.jbox-web.fr>
2015-08-27 18:21:48 +0200

    Extract method

--- lib/redmine_git_hosting/patches/settings_controller_patch.rb
+++ lib/redmine_git_hosting/patches/settings_controller_patch.rb
@@ -33,16 +33,7 @@ module RedmineGitHosting
           @plugin = Redmine::Plugin.find(params[:id])
           return plugin_without_git_hosting(&block) unless @plugin.id == :redmine_git_hosting
           if request.post?
-            @settings_form = PluginSettingsForm.new(@plugin)
-            options = params[:settings].delete(:rescue){ {} }
-            if @settings_form.submit(params[:settings])
-              @old_settings = Setting.send("plugin_#{@plugin.id}")
-              Setting.send "plugin_#{@plugin.id}=", @settings_form.params
-              execute_post_actions(@old_settings, options)
-              flash[:notice] = l(:notice_successful_update)
-            else
-              flash[:error] = @settings_form.errors.full_messages.join('<br>')
-            end
+            handle_settings_update
             redirect_to plugin_settings_path(@plugin)
           else
             @partial = @plugin.settings[:partial]
@@ -52,6 +43,31 @@ module RedmineGitHosting
 
 
         private
+
+
+          def handle_settings_update
+            # Create FormObject
+            settings_form = PluginSettingsForm.new(@plugin)
+
+            # Strip *rescue* hash from params as we don't want to save them
+            options = params[:settings].delete(:rescue){ {} }
+
+            # Validate form
+            if settings_form.submit(params[:settings])
+              # Backup old settings
+              old_settings = Setting.send("plugin_#{@plugin.id}")
+
+              # Save settings for real
+              Setting.send "plugin_#{@plugin.id}=", settings_form.params
+
+              # Execute post actions
+              execute_post_actions(old_settings, options)
+
+              flash[:notice] = l(:notice_successful_update)
+            else
+              flash[:error] = settings_form.errors.full_messages.join('<br>')
+            end
+          end
 
 
           def execute_post_actions(old_settings, opts = {})
