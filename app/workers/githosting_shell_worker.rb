f9b15535e07678334abd27e6b093942b7321ae42
root <root@dev.jbox-web.fr>
2015-08-27 18:49:40 +0200

    Fix Sidekiq worker

--- app/workers/githosting_shell_worker.rb
+++ app/workers/githosting_shell_worker.rb
@@ -1,10 +1,10 @@
 class GithostingShellWorker
   include Sidekiq::Worker
 
-  sidekiq_options :queue => :redmine_git_hosting, :retry => false
+  sidekiq_options queue: :redmine_git_hosting, retry: false
 
   def perform(command, object, options = {})
     logger.info("#{command} | #{object} | #{options}")
-    RedmineGitHosting::GitoliteWrapper.resync_gitolite(command, object, options)
+    GitoliteWrapper.resync_gitolite(command, object, options)
   end
 end
