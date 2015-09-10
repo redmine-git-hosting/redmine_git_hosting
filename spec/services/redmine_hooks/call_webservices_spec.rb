310928b89a09fa0f37a346ac384310b149344a8a
root <root@dev.jbox-web.fr>
2015-09-10 23:20:28 +0200

    Remove before block in spec

--- spec/services/redmine_hooks/call_webservices_spec.rb
+++ spec/services/redmine_hooks/call_webservices_spec.rb
@@ -7,66 +7,49 @@ describe RedmineHooks::CallWebservices do
   BRANCHES_PAYLOAD = YAML::load(File.open(File.expand_path(File.dirname(__FILE__) + '/../../fixtures/branches_payload.yml')))
 
 
-  describe "#needs_push" do
-    before do
-      @post_receive_url = build(:repository_post_receive_url)
-    end
+  describe '#needs_push' do
+    let(:post_receive_url){ build(:repository_post_receive_url) }
 
-    context "when payload is empty" do
-      before do
-        @web_hook = RedmineHooks::CallWebservices.new(@post_receive_url, [])
-      end
-
-      it "shoud return false" do
-        expect(@web_hook.needs_push?).to be false
+    context 'when payload is empty' do
+      it 'shoud return false' do
+        web_hook = RedmineHooks::CallWebservices.new(post_receive_url, [])
+        expect(web_hook.needs_push?).to be false
       end
     end
 
-    context "when triggers are not used" do
-      before do
-        @web_hook = RedmineHooks::CallWebservices.new(@post_receive_url, GLOBAL_PAYLOAD)
-      end
-
-      it "should return the global payload to push" do
-        expect(@web_hook.needs_push?).to be true
-        expect(@web_hook.payloads_to_send).to eq GLOBAL_PAYLOAD
+    context 'when triggers are not used' do
+      it 'should return the global payload to push' do
+        web_hook = RedmineHooks::CallWebservices.new(post_receive_url, GLOBAL_PAYLOAD)
+        expect(web_hook.needs_push?).to be true
+        expect(web_hook.payloads_to_send).to eq GLOBAL_PAYLOAD
       end
     end
 
-    context "when triggers are empty" do
-      before do
-        @post_receive_url.use_triggers = true
-        @web_hook = RedmineHooks::CallWebservices.new(@post_receive_url, GLOBAL_PAYLOAD)
-      end
-
-      it "should return the global payload to push" do
-        expect(@web_hook.needs_push?).to be false
-        expect(@web_hook.payloads_to_send).to eq []
+    context 'when triggers are empty' do
+      it 'should return the global payload to push' do
+        post_receive_url.use_triggers = true
+        web_hook = RedmineHooks::CallWebservices.new(post_receive_url, GLOBAL_PAYLOAD)
+        expect(web_hook.needs_push?).to be false
+        expect(web_hook.payloads_to_send).to eq []
       end
     end
 
-    context "when triggers is set to master" do
-      before do
-        @post_receive_url.use_triggers = true
-        @post_receive_url.triggers = ['master']
-        @web_hook = RedmineHooks::CallWebservices.new(@post_receive_url, GLOBAL_PAYLOAD)
-      end
-
-      it "should return the master payload" do
-        expect(@web_hook.needs_push?).to be true
-        expect(@web_hook.payloads_to_send).to eq MASTER_PAYLOAD
+    context 'when triggers is set to master' do
+      it 'should return the master payload' do
+        post_receive_url.use_triggers = true
+        post_receive_url.triggers = ['master']
+        web_hook = RedmineHooks::CallWebservices.new(post_receive_url, GLOBAL_PAYLOAD)
+        expect(web_hook.needs_push?).to be true
+        expect(web_hook.payloads_to_send).to eq MASTER_PAYLOAD
       end
     end
 
-    context "when triggers is set to master" do
-      before do
-        @post_receive_url.use_triggers = true
-        @post_receive_url.triggers = ['master']
-        @web_hook = RedmineHooks::CallWebservices.new(@post_receive_url, BRANCHES_PAYLOAD)
-      end
-
-      it "should not be found in branches payload and return false" do
-        expect(@web_hook.needs_push?).to be false
+    context 'when triggers is set to master' do
+      it 'should not be found in branches payload and return false' do
+        post_receive_url.use_triggers = true
+        post_receive_url.triggers = ['master']
+        web_hook = RedmineHooks::CallWebservices.new(post_receive_url, BRANCHES_PAYLOAD)
+        expect(web_hook.needs_push?).to be false
       end
     end
   end
