433f529f803371855ab343922633c631b6886345
root <root@dev.jbox-web.fr>
2015-09-10 22:23:27 +0200

    Fix regex security warning

--- app/forms/plugin_settings_form.rb
+++ app/forms/plugin_settings_form.rb
@@ -1,7 +1,7 @@
 class PluginSettingsForm
   unloadable
 
-  DOMAIN_REGEX   = /^[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)*(:\d+)?$/i
+  DOMAIN_REGEX   = /\A[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)*(:\d+)?\z/i
   EMAIL_REGEX    = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   BOOLEAN_FIELDS = ['true', 'false']
   CACHE_ADAPTERS = ['database', 'memcached', 'redis']
