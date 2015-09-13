3d45e74795d33649b853c472d57574023af7a6ca
root <root@dev.jbox-web.fr>
2015-09-13 02:18:41 +0200

    Some methods should be private

--- lib/redmine_git_hosting/redmine_plugin_loader.rb
+++ lib/redmine_git_hosting/redmine_plugin_loader.rb
@@ -22,26 +22,6 @@ module RedmineGitHosting
     end
 
 
-    def plugin_dir(*dirs)
-      Rails.root.join('plugins', plugin_name, *dirs)
-    end
-
-
-    def plugin_app_dir(*dirs)
-      plugin_dir('app', *dirs)
-    end
-
-
-    def plugin_conf_dir(*dirs)
-      plugin_dir('config', *dirs)
-    end
-
-
-    def plugin_lib_dir(*dirs)
-      plugin_dir('lib', *dirs)
-    end
-
-
     def plugin_patches_dir
       plugin_lib_dir(plugin_name, 'patches')
     end
@@ -69,29 +49,52 @@ module RedmineGitHosting
     end
 
 
-    def hook_file?(file)
-      File.dirname(file) == plugin_hooks_dir.to_s
-    end
+    private
 
 
-    def autoload_libs!
-      Dir.glob(required_lib_dirs).each do |file|
-        # Exclude Redmine Views Hooks from Rails loader to avoid multiple calls to hooks on reload in dev environment.
-        require_dependency file unless hook_file?(file)
+      def plugin_dir(*dirs)
+        Rails.root.join('plugins', plugin_name, *dirs)
       end
-    end
 
 
-    def autoload_paths!
-      autoloaded_paths.each do |dir|
-        ActiveSupport::Dependencies.autoload_paths += [dir] if Dir.exists?(dir)
+      def plugin_app_dir(*dirs)
+        plugin_dir('app', *dirs)
       end
-    end
 
 
-    def autoload_locales!
-      ::I18n.load_path += Dir.glob(plugin_locales_dir)
-    end
+      def plugin_conf_dir(*dirs)
+        plugin_dir('config', *dirs)
+      end
+
+
+      def plugin_lib_dir(*dirs)
+        plugin_dir('lib', *dirs)
+      end
+
+
+      def hook_file?(file)
+        File.dirname(file) == plugin_hooks_dir.to_s
+      end
+
+
+      def autoload_libs!
+        Dir.glob(required_lib_dirs).each do |file|
+          # Exclude Redmine Views Hooks from Rails loader to avoid multiple calls to hooks on reload in dev environment.
+          require_dependency file unless hook_file?(file)
+        end
+      end
+
+
+      def autoload_paths!
+        autoloaded_paths.each do |dir|
+          ActiveSupport::Dependencies.autoload_paths += [dir] if Dir.exists?(dir)
+        end
+      end
+
+
+      def autoload_locales!
+        ::I18n.load_path += Dir.glob(plugin_locales_dir)
+      end
 
   end
 end
