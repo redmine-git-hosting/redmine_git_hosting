280ea465d72c4f0330281b5123b7ade539a7aa52
root <root@dev.jbox-web.fr>
2015-09-16 00:26:20 +0200

    Extract method

--- contrib/hooks/post-receive/lib/git_hosting_post_receive.rb
+++ contrib/hooks/post-receive/lib/git_hosting_post_receive.rb
@@ -27,22 +27,9 @@ module GitHosting
         logger.info('')
         logger.info("Notifying Redmine about changes to this repository : '#{git_config.repository_name}' ...")
 
-        opts = {}
-        opts[:params] = http_post_data
-
-        http_post(git_config.project_url, opts) do |http, request|
+        http_post(git_config.project_url, { params: http_post_data }) do |http, request|
           begin
-            http.request(request) do |response|
-              if response.code.to_i == 200
-                response.read_body do |body_frag|
-                  body_frag.split("\n").each do |line|
-                    logger.info(line)
-                  end
-                end
-              else
-                logger.error("  - Error while notifying Redmine ! (status code: #{response.code})")
-              end
-            end
+            http.request(request) { |response| check_response(response) }
           rescue => e
             logger.error("HTTP_ERROR : #{e.message}")
           end
@@ -64,6 +51,17 @@ module GitHosting
           parsed << [r[0].to_s, r[1].to_s, r[2].to_s].join(',')
         end
         parsed
+      end
+
+
+      def check_response(response)
+        if response.code.to_i == 200
+          response.read_body do |body_frag|
+            body_frag.split("\n").each { |line| logger.info(line) }
+          end
+        else
+          logger.error("  - Error while notifying Redmine ! (status code: #{response.code})")
+        end
       end
 
 
