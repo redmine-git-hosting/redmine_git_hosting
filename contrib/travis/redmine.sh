d3d32347acd19354f7d7ebd0bcaa86d69bb49549
root <root@dev.jbox-web.fr>
2016-12-27 01:04:11 +0100

    Get Redmine sources from Github

--- contrib/travis/redmine.sh
+++ contrib/travis/redmine.sh
@@ -2,6 +2,7 @@
 
 REDMINE_PACKAGE_URL="http://www.redmine.org/releases"
 REDMINE_SVN_URL="https://svn.redmine.org/redmine/branches"
+REDMINE_GIT_URL="https://github.com/redmine/redmine.git"
 
 REDMINE_NAME="redmine-${REDMINE_VERSION}"
 REDMINE_PACKAGE="${REDMINE_NAME}.tar.gz"
@@ -19,7 +20,8 @@ function install_redmine() {
   install_redmine_libs
 
   if [ $USE_SVN == 'true' ] ; then
-    install_redmine_from_svn
+    # install_redmine_from_svn
+    install_redmine_from_git
   else
     install_redmine_from_package
   fi
@@ -59,5 +61,15 @@ function install_redmine_from_package() {
 function install_redmine_from_svn() {
   log_title "GET SOURCES FROM SVN"
   svn co --non-interactive --trust-server-cert "${REDMINE_SVN_URL}/${REDMINE_VERSION}" "${REDMINE_NAME}"
+  log_ok
+}
+
+
+function install_redmine_from_git() {
+  log_title "GET SOURCES FROM GIT"
+  git clone "${REDMINE_GIT_URL}" "${REDMINE_NAME}"
+  pushd "${REDMINE_NAME}"
+  git checkout "${REDMINE_VERSION}"
+  popd
   log_ok
 }
