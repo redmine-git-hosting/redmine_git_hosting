05798f85f47747d279635c0fd2291d36a969a44a
root <root@dev.jbox-web.fr>
2016-12-31 03:57:16 +0100

    Get Gitolite lib dir with gitolite query-rc command

--- app/forms/plugin_settings_validation/storage_config.rb
+++ app/forms/plugin_settings_validation/storage_config.rb
@@ -9,18 +9,16 @@ module PluginSettingsValidation
       add_accessor :gitolite_global_storage_dir,
                    :gitolite_redmine_storage_dir,
                    :gitolite_recycle_bin_dir,
-                   :gitolite_local_code_dir,
-                   :gitolite_lib_dir
+                   :gitolite_local_code_dir
 
       before_validation do
         self.gitolite_global_storage_dir  = strip_value(gitolite_global_storage_dir)
         self.gitolite_redmine_storage_dir = strip_value(gitolite_redmine_storage_dir)
         self.gitolite_recycle_bin_dir     = strip_value(gitolite_recycle_bin_dir)
         self.gitolite_local_code_dir      = strip_value(gitolite_local_code_dir)
-        self.gitolite_lib_dir             = strip_value(gitolite_lib_dir)
       end
 
-      validates_presence_of :gitolite_global_storage_dir, :gitolite_recycle_bin_dir, :gitolite_lib_dir
+      validates_presence_of :gitolite_global_storage_dir, :gitolite_recycle_bin_dir
       validates_presence_of :gitolite_local_code_dir, if: Proc.new { RedmineGitHosting::Config.gitolite_version == 3 }
 
       validates_each PATHS_TO_VALIDATE do |record, attr, value|
