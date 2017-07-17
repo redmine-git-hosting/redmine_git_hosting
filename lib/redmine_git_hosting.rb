4626d4b2c9026d75fa0a8290c212feec3b5e15ff
Nicolas Rodriguez <nicoladmin@free.fr>
2017-07-17 12:17:06 +0200

    Fix plugin icon with Redmine 3.4.x

--- lib/redmine_git_hosting.rb
+++ lib/redmine_git_hosting.rb
@@ -3,7 +3,7 @@ require 'redmine_permissions'
 
 ## Redmine Menus
 Redmine::MenuManager.map :admin_menu do |menu|
-  menu.push :redmine_git_hosting, { controller: 'settings', action: 'plugin', id: 'redmine_git_hosting' }, caption: :redmine_git_hosting
+  menu.push :redmine_git_hosting, { controller: 'settings', action: 'plugin', id: 'redmine_git_hosting' }, caption: :redmine_git_hosting, html: { class: 'icon' }
 end
 
 Redmine::MenuManager.map :top_menu do |menu|
