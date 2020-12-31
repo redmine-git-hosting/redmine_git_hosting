44b7dab9737131d948a02c9eda81bd87c0932f9c
Alexander Meindl <alex@alphanodes.com>
2020-12-31 06:56:16 +0100

    Fix loading problem without Journald #776

--- lib/redmine_git_hosting/journal_logger.rb
+++ lib/redmine_git_hosting/journal_logger.rb
@@ -1,35 +1,37 @@
 module RedmineGitHosting
   # @see https://github.com/theforeman/journald-logger
-  class JournalLogger < ::Journald::Logger
-    def self.init_logs!(progname, loglevel)
-      logger = new progname, type: progname
-      logger.level = loglevel
+  if defined? ::Journald::Logger
+    class JournalLogger < ::Journald::Logger
+      def self.init_logs!(progname, loglevel)
+        logger = new progname, type: progname
+        logger.level = loglevel
 
-      logger
-    end
+        logger
+      end
 
-    def debug(msg)
-      super msg2str(msg)
-    end
+      def debug(msg)
+        super msg2str(msg)
+      end
 
-    def info(msg)
-      super msg2str(msg)
-    end
+      def info(msg)
+        super msg2str(msg)
+      end
 
-    def warn(msg)
-      super msg2str(msg)
-    end
+      def warn(msg)
+        super msg2str(msg)
+      end
 
-    def error(msg)
-      super msg2str(msg)
-    end
+      def error(msg)
+        super msg2str(msg)
+      end
 
-    def msg2str(msg)
-      case msg
-      when ::String
-        msg
-      else
-        msg.inspect
+      def msg2str(msg)
+        case msg
+        when ::String
+          msg
+        else
+          msg.inspect
+        end
       end
     end
   end
