a8f0b430c231376057dacb29cfa79be092f89eeb
Alexander Meindl <alex@alphanodes.com>
2021-01-10 18:25:59 +0100

    Show correct mode for empty post post_receive hook

--- app/helpers/repository_post_receive_urls_helper.rb
+++ app/helpers/repository_post_receive_urls_helper.rb
@@ -5,6 +5,8 @@ module RepositoryPostReceiveUrlsHelper
     if prurl.github_mode?
       label << l(:label_github_post)
       label << "(#{l :label_split_payloads})" if prurl.split_payloads?
+    elsif prurl.mode == :post
+      label << l(:label_empty_post)
     else
       label << l(:label_empty_get)
     end
