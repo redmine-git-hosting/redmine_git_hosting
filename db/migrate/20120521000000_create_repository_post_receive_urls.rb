24451d48f960cf5ec6ac46b3bcee5092602b31bb
Alexander Meindl <alex@alphanodes.com>
2021-04-09 23:52:42 +0200

    Fix migration problems for Redmine master

--- db/migrate/20120521000000_create_repository_post_receive_urls.rb
+++ db/migrate/20120521000000_create_repository_post_receive_urls.rb
@@ -1,10 +1,9 @@
 class CreateRepositoryPostReceiveUrls < ActiveRecord::Migration[4.2]
   def change
     create_table :repository_post_receive_urls do |t|
-      t.column :project_id, :integer
+      t.references :project, type: :integer
       t.column :active, :integer, default: 1
       t.column :url, :string
-      t.references :project
       t.timestamps
     end
   end
