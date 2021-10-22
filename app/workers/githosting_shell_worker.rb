e4b2bab17ccb0cb4f2c6719fcc46ff695cd96dbf
Alexander Meindl <alex@alphanodes.com>
2021-10-22 08:35:53 +0200

    #800 fix resync_gitolite call for async job

--- app/workers/githosting_shell_worker.rb
+++ app/workers/githosting_shell_worker.rb
@@ -16,6 +16,6 @@ class GithostingShellWorker
 
   def perform(command, object, options)
     logger.info "#{command} | #{object} | #{options}"
-    RedmineGitHosting::GitoliteWrapper.resync_gitolite command, object, options
+    RedmineGitHosting::GitoliteWrapper.resync_gitolite command, object, **options
   end
 end
